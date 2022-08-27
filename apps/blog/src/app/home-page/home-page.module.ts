805f2ecee8085c327f723270ede3a214f5ae24f3
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2022-08-27 17:51:23 -0500

    update first section

--- apps/blog/src/app/home-page/home-page.module.ts
+++ apps/blog/src/app/home-page/home-page.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HomePageComponent } from "./home-page.component";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
 })
 export class HomePageModule {}
