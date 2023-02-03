3443bec4e66c0ff588c84f4c2d945c5fd78f1b76
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2023-02-02 22:30:16 -0600

    add carosel

--- apps/blog/src/app/home-page/home-page.module.ts
+++ apps/blog/src/app/home-page/home-page.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { HomePageComponent } from "./home-page.component";
+import { NgModule } from "@angular/core";
+
+import { IvyCarouselModule } from "angular-responsive-carousel";
 import { SharedModule } from "../shared/shared.module";
+import { HomePageComponent } from "./home-page.component";
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, IvyCarouselModule],
 })
 export class HomePageModule {}
--- apps/blog/src/app/shared/shared.module.ts
+++ apps/blog/src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { TextEditorComponent } from "./components/text-editor/text-editor.component";
-import { NgxEditorModule } from "ngx-editor";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ReferenceTooltipComponent } from "./components/reference-tooltip/reference-tooltip.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { NgxEditorModule } from "ngx-editor";
+import { ReferenceTooltipComponent } from "./components/reference-tooltip/reference-tooltip.component";
+import { TextEditorComponent } from "./components/text-editor/text-editor.component";
 
 @NgModule({
   declarations: [TextEditorComponent, ReferenceTooltipComponent],
