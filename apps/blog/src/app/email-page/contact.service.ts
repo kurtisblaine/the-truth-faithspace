3ac302859e0eda8eacff69eef7281f1af5f220ba
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2023-03-11 23:29:04 -0600

    add email page (no style yet)

---
+++ apps/blog/src/app/email-page/contact.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ContactService {
+  public api = "https://mailthis.to/kurtisblainewaldner@gmail.com";
+
+  constructor(private http: HttpClient) {}
+
+  public postMessage(input: any) {
+    return this.http.post(this.api, input, { responseType: "text" }).pipe(
+      map((response: any) => {
+        if (response) {
+          return response;
+        }
+      })
+    );
+  }
+}
