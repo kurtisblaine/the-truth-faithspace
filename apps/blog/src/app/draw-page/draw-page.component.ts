aafab12d3e8b84328eafb815c09ba862de369f26
Kurtis Blaine Waldner <kurt.waldner@midco.com>
2023-11-05 15:32:33 -0600

    add draw page

--- apps/blog/src/app/draw-page/draw-page.component.ts
+++ apps/blog/src/app/draw-page/draw-page.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from "@angular/core";
-
+interface Image {
+  name?: string;
+  description?: string;
+  thumbnailUrl?: string;
+  originUrl: string;
+}
 @Component({
   selector: "blog-draw-page",
   templateUrl: "./draw-page.component.html",
   styleUrls: ["./draw-page.component.scss"],
 })
 export class DrawPageComponent implements OnInit {
+  public images: Image[] = [];
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.images.push({ name: "", originUrl: "" });
+  }
 }
