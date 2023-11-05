2a7e1f6f766fafe8362945eee88a4f79408f0dd0
Kurtis Blaine Waldner <kurt.waldner@midco.com>
2023-11-04 20:15:54 -0500

    add page

---
+++ apps/blog/src/app/draw-page/draw-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "blog-draw-page",
+  templateUrl: "./draw-page.component.html",
+  styleUrls: ["./draw-page.component.scss"],
+})
+export class DrawPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
