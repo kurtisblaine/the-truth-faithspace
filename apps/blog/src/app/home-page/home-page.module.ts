c439c42e200df98ad92cad4223f40b8410cd0ca7
Kurtis Blaine Waldner <kurt.waldner@midco.com>
2023-11-21 12:36:33 -0600

    bring back carosel

--- apps/blog/src/app/home-page/home-page.module.ts
+++ apps/blog/src/app/home-page/home-page.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
-// import { IvyCarouselModule } from "angular-responsive-carousel";
 import { MatButtonModule } from "@angular/material/button";
+import { IvyCarouselModule } from "angular-responsive-carousel2";
 import { SharedModule } from "../shared/shared.module";
 import { HomePageComponent } from "./home-page.component";
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, SharedModule, MatButtonModule],
-  // IvyCarouselModule, not compatable with new angular version
+  imports: [CommonModule, SharedModule, MatButtonModule, IvyCarouselModule],
 })
 export class HomePageModule {}
