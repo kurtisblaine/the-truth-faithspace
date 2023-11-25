b1f0e6f21f337d602dd9b2e9cea56f073ea3031f
Kurtis Blaine Waldner <kurt.waldner@midco.com>
2023-11-24 18:49:03 -0600

    message

--- apps/blog/src/app/tract-page/tract-page.component.ts
+++ apps/blog/src/app/tract-page/tract-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "blog-tract-page",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: "./tract-page.component.html",
   styleUrl: "./tract-page.component.css",
 })
