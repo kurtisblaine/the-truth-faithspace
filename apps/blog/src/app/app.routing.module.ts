3db3c9ad3bde0f8cc58178fc3a1e90b349e1e42b
Kurtis Blaine Waldner <kurt.waldner@midco.com>
2023-12-31 22:33:05 -0600

    last bit of updates

--- apps/blog/src/app/app.routing.module.ts
+++ apps/blog/src/app/app.routing.module.ts
@@ -73,6 +73,7 @@ const routes: Routes = [
       onSameUrlNavigation: "reload",
       anchorScrolling: "enabled",
       preloadingStrategy: PreloadAllModules,
+      scrollPositionRestoration: "enabled",
     }),
   ],
   exports: [RouterModule],
