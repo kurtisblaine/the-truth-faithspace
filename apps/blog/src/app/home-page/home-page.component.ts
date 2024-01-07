7be70698fe4da69d5724d5b587870d806c4a2eab
Kurtis Blaine Waldner <kurt.waldner@midco.com>
2024-01-06 21:52:58 -0600

    try

--- apps/blog/src/app/home-page/home-page.component.ts
+++ apps/blog/src/app/home-page/home-page.component.ts
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  EnvironmentInjector,
   HostBinding,
   OnInit,
+  ViewChild,
 } from "@angular/core";
+import { FullpageDirective } from "@fullpage/angular-fullpage";
+import {
+  fullpageApi,
+  fullpageOptions,
+} from "fullpage.js/dist/fullpage.extensions.min";
 import { fadeInOut } from "../shared/animations/animation";
-// import * as Parallax from "parallax-js";
 
 @Component({
   selector: "blog-home-page",
@@ -14,16 +22,47 @@ import { fadeInOut } from "../shared/animations/animation";
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [fadeInOut],
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit {
   @HostBinding("id.scrollProgress") someField = true;
+  public config: fullpageOptions;
+  public fullpageApi: fullpageApi;
 
-  constructor() {}
+  @ViewChild(FullpageDirective)
+  fullpageDirective: FullpageDirective;
 
-  ngOnInit(): void {
-    //KEEP THIS JUST IN CASE IT's NEEDED
-    // const Parallax = require("parallax-js");
-    // const scene = $(".scene").get(0);
-    // const parallaxInstance = new Parallax(scene, {});
-    // parallaxInstance.friction(0.2, 0.2);
+  constructor(private injector: EnvironmentInjector) {
+    this.config = {
+      licenseKey: "GM477-9I82I-1L8K9-194JK-TJUVR",
+      anchors: [
+        "firstPage",
+        "secondPage",
+        "thirdPage",
+        "fourthPage",
+        "lastPage",
+      ],
+      credits: { enabled: false },
+      // menu: "#menu",
+      navigation: true,
+      sectionsColor: ["#1bbc9b", "#4BBFC3", "#7BAABE", "whitesmoke", "#ccddff"],
+      // events callback
+      afterLoad: (origin, destination, direction) => {
+        // console.log(destination);
+      },
+      afterRender: () => {
+        // console.log('afterRender');
+      },
+      afterResize: (width, height) => {
+        // console.log('afterResize' + width + ' ' + height);
+      },
+      afterSlideLoad: (section, origin, destination, direction) => {
+        // console.log(destination);
+      },
+    };
   }
+
+  ngAfterViewInit() {
+    this.fullpageApi = this.fullpageDirective.fullpageApi;
+  }
+
+  ngOnInit(): void {}
 }
--- apps/blog/src/app/home-page/home-page.module.ts
+++ apps/blog/src/app/home-page/home-page.module.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { CommonModule } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
+import { AngularFullpageModule } from "@fullpage/angular-fullpage";
 import { IvyCarouselModule } from "angular-responsive-carousel2";
 import { SharedModule } from "../shared/shared.module";
 import { HomePageComponent } from "./home-page.component";
+
 const routes: Routes = [
   {
     path: "",
@@ -20,6 +22,7 @@ const routes: Routes = [
     SharedModule,
     MatButtonModule,
     IvyCarouselModule,
+    AngularFullpageModule,
     RouterModule.forChild(routes),
   ],
 })
