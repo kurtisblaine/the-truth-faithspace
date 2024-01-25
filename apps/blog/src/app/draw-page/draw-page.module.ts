b4d532fd467f3185d785633dc0141baa34eeb1f5
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-01-24 19:33:24 -0600

    update styles and add youtube component

--- apps/blog/src/app/draw-page/draw-page.module.ts
+++ apps/blog/src/app/draw-page/draw-page.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from "@angular/core";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { RouterModule, Routes } from "@angular/router";
+import { YOUTUBE_PLAYER_CONFIG, YouTubePlayer } from "@angular/youtube-player";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { PhotoGalleryModule } from "@twogate/ngx-photo-gallery";
 import { DrawItemComponent } from "./draw-item/draw-item.component";
 import { DrawPageComponent } from "./draw-page.component";
+
 const routes: Routes = [
   {
     path: "drawing",
@@ -14,12 +17,22 @@ const routes: Routes = [
   { path: "draw/:id", component: DrawItemComponent },
 ];
 @NgModule({
-  declarations: [DrawPageComponent],
+  declarations: [DrawPageComponent, DrawItemComponent],
+  providers: [
+    {
+      provide: YOUTUBE_PLAYER_CONFIG,
+      useValue: {
+        disablePlaceholder: true,
+      },
+    },
+  ],
   imports: [
     CommonModule,
     MatProgressSpinnerModule,
     RouterModule.forChild(routes),
     MatGridListModule,
+    YouTubePlayer,
+    FontAwesomeModule,
     PhotoGalleryModule.forRoot({
       defaultOptions: {
         arrowEl: false,
