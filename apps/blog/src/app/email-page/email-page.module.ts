6c0c5c05bd43716c85912e8c31af60f0c35a0c53
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-03-08 22:44:51 -0600

    efficency

--- apps/blog/src/app/email-page/email-page.module.ts
+++ apps/blog/src/app/email-page/email-page.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
@@ -19,6 +20,7 @@ const routes: Routes = [
   declarations: [EmailPageComponent],
   imports: [
     RouterModule.forChild(routes),
+    MatButtonModule,
     MatSnackBarModule,
     CommonModule,
     ReactiveFormsModule,
--- apps/template/src/app/state/template/template.models.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Interface for the 'template' data
- */
-export interface TemplateEntity {
-  id: string | number; // Primary ID
-  name: string;
-}
