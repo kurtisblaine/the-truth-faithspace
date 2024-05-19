106a6bdda4f03d9817609014d7aefa32824a54b1
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-05-19 18:02:12 -0500

    show carousel again

--- apps/blog/src/app/gospel-page/gospel-page.module.ts
+++ apps/blog/src/app/gospel-page/gospel-page.module.ts
@@ -2,9 +2,9 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { RouterModule, Routes } from "@angular/router";
+import { IvyCarouselModule } from "@degloman/angular-responsive-carousel";
 import { SharedModule } from "../shared/shared.module";
 import { GospelComponent } from "./gospel/gospel.component";
-// import { IvyCarouselModule } from "angular-responsive-carousel2";
 
 const routes: Routes = [
   {
@@ -16,7 +16,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [GospelComponent],
   imports: [
-    // IvyCarouselModule,
+    IvyCarouselModule,
     CommonModule,
     SharedModule,
     MatButtonModule,
