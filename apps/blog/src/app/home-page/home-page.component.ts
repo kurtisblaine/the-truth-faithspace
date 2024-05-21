5b3dbae65b7c200c94b61929e84233075df5ddd2
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-05-20 22:28:33 -0500

    make updates and release

--- apps/blog/src/app/home-page/home-page.component.ts
+++ apps/blog/src/app/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { fadeInOut } from "../shared/animations/animation";
 
 @Component({
   selector: "blog-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeInOut],
 })
 export default class HomePageComponent implements OnInit {
   constructor() {}
