9f0233ccdf8856d932fab3d84b4f34ddbc14c06f
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-05-25 14:07:56 -0500

    add angular material themeing

--- apps/bible/src/app/app.component.ts
+++ apps/bible/src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   selector: "app-root",
   template: `<h1>Welcome Bible</h1>
     <router-outlet></router-outlet>`,
