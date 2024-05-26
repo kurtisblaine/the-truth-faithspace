3ba69698c066b470787dcc8316b443000a661b3e
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-05-25 19:44:28 -0500

    make to show info on grid

--- apps/bible/src/app/books-page/books-page.component.ts
+++ apps/bible/src/app/books-page/books-page.component.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import { MatExpansionModule } from "@angular/material/expansion";
+import { MatGridListModule } from "@angular/material/grid-list";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
+import * as _ from "lodash-es";
 import { Observable, map } from "rxjs";
 import { initBooks } from "../+state/book/books.actions";
 import {
   selectAllGroupedLanguages,
   selectBooksLoaded,
 } from "../+state/book/books.selectors";
-import { ScriptDirection, SortedBooks } from "../+state/models/bibles";
-
+import {
+  BibleBook,
+  ScriptDirection,
+  SortedBooks,
+} from "../+state/models/bibles";
 @Component({
   selector: "app-books-page",
   standalone: true,
@@ -21,6 +27,8 @@ import { ScriptDirection, SortedBooks } from "../+state/models/bibles";
     CommonModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    MatGridListModule,
+    MatCardModule,
   ],
   templateUrl: "./books-page.component.html",
   styleUrl: "./books-page.component.scss",
@@ -28,7 +36,7 @@ import { ScriptDirection, SortedBooks } from "../+state/models/bibles";
 })
 export class BooksPageComponent implements OnInit {
   public rtl: ScriptDirection = "RTL";
-  public books$!: Observable<SortedBooks[]>;
+  public groups$!: Observable<SortedBooks[]>;
   public isLoading$!: Observable<boolean>;
 
   constructor(private store: Store, private router: Router) {}
@@ -36,7 +44,19 @@ export class BooksPageComponent implements OnInit {
   public ngOnInit() {
     this.store.dispatch(initBooks());
 
-    this.books$ = this.store.select(selectAllGroupedLanguages);
+    this.groups$ = this.store.select(selectAllGroupedLanguages);
     this.isLoading$ = this.store.select(selectBooksLoaded).pipe(map((r) => !r));
+  }
+
+  public getCountries(group: BibleBook[]) {
+    const flattened = _.flatMap(group, (g) => g.countries);
+    const dedupped = [...new Set(flattened.map((f) => f.name))];
+    return dedupped.join(", ");
+  }
+
+  public getScripts(group: BibleBook[]) {
+    const flattened = _.flatMap(group, (g) => g.language);
+    const dedupped = [...new Set(flattened.map((f) => f.script))];
+    return dedupped.join(", ");
   }
 }
