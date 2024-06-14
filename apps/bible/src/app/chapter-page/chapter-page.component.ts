b24670224ec4e2601426114b6206122e5b91c027
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-06-14 16:45:01 -0500

    add all chapter

--- apps/bible/src/app/chapter-page/chapter-page.component.ts
+++ apps/bible/src/app/chapter-page/chapter-page.component.ts
@@ -1,32 +1,19 @@
 import { CommonModule } from "@angular/common";
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input as RouteInput,
-} from "@angular/core";
-import { MatCardModule } from "@angular/material/card";
-import { MatRippleModule } from "@angular/material/core";
+import { ChangeDetectionStrategy, Component, Input as RouteInput } from "@angular/core";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { Observable, map } from "rxjs";
 import { selectEntity } from "../+state/books/books.selectors";
 import { ChaptersActions } from "../+state/chapters/chapters.actions";
-import {
-  selectAllChapters,
-  selectChaptersLoaded,
-} from "../+state/chapters/chapters.selectors";
+import { selectAllChapters, selectChaptersLoaded } from "../+state/chapters/chapters.selectors";
 import { Chapter } from "../models/chapters";
+import { ChapterItemComponent } from "./chapter-item/chapter-item.component";
 
 @Component({
   selector: "app-chapter-page",
   standalone: true,
-  imports: [
-    MatProgressSpinnerModule,
-    CommonModule,
-    MatCardModule,
-    MatRippleModule,
-  ],
+  imports: [MatProgressSpinnerModule, CommonModule, ChapterItemComponent],
   templateUrl: "./chapter-page.component.html",
   styleUrl: "./chapter-page.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -38,23 +25,22 @@ export class ChapterPageComponent {
 
   public chapters$!: Observable<Chapter[]>;
   public isLoading$!: Observable<boolean>;
+  public isLoaded$!: Observable<boolean>;
   public selectedBook$!: Observable<string>;
+  public allChapter!: Chapter;
 
   constructor(private store: Store, private router: Router) {}
 
   ngOnInit() {
-    this.store.dispatch(
-      ChaptersActions.loadChapters({ id: this.bibleId, bookId: this.bookId })
-    );
+    this.allChapter = { number: "All", bibleId: this.bibleId, bookId: this.bookId, id: "777" } as Chapter;
+
+    this.store.dispatch(ChaptersActions.loadChapters({ id: this.bibleId, bookId: this.bookId }));
 
     this.chapters$ = this.store.select(selectAllChapters);
-    this.selectedBook$ = this.store
-      .select(selectEntity)
-      .pipe(map((r) => r.name));
+    this.selectedBook$ = this.store.select(selectEntity).pipe(map((r) => r.name));
 
-    this.isLoading$ = this.store
-      .select(selectChaptersLoaded)
-      .pipe(map((r) => !r));
+    this.isLoading$ = this.store.select(selectChaptersLoaded).pipe(map((r) => !r));
+    this.isLoaded$ = this.store.select(selectChaptersLoaded);
   }
 
   getScripture(chapter: Chapter) {
