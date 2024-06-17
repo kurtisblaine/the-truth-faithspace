df8925065f4926f3775bb5fd41ca241a60428aa4
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-06-16 21:14:26 -0500

    add to show error when down

--- apps/bible/src/app/book-page/book-page.component.ts
+++ apps/bible/src/app/book-page/book-page.component.ts
@@ -2,18 +2,19 @@ import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component, OnInit, Input as RouteInput } from "@angular/core";
 import { MatListModule } from "@angular/material/list";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSnackBar, MatSnackBarModule } from "@angular/material/snack-bar";
 import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { Observable, map } from "rxjs";
 import { selectTranslationEntity } from "../+state/bibles/bibles.selectors";
 import { BooksActions } from "../+state/books/books.actions";
-import { selectAllBooks, selectBooksLoaded } from "../+state/books/books.selectors";
+import { selectAllBooks, selectBooksError, selectBooksLoaded } from "../+state/books/books.selectors";
 import { Book } from "../models/books";
 
 @Component({
   selector: "app-book-page",
   standalone: true,
-  imports: [MatProgressSpinnerModule, CommonModule, MatListModule],
+  imports: [MatProgressSpinnerModule, CommonModule, MatListModule, MatSnackBarModule],
   templateUrl: "./book-page.component.html",
   styleUrl: "./book-page.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,7 +27,7 @@ export class BookPageComponent implements OnInit {
   public isLoading$!: Observable<boolean>;
   public translation$!: Observable<string>;
 
-  constructor(private store: Store, private router: Router) {}
+  constructor(private store: Store, private router: Router, private _snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.store.dispatch(BooksActions.loadBooks({ id: this.bibleId }));
@@ -34,6 +35,14 @@ export class BookPageComponent implements OnInit {
     this.books$ = this.store.select(selectAllBooks);
     this.isLoading$ = this.store.select(selectBooksLoaded).pipe(map((r) => !r));
     this.translation$ = this.store.select(selectTranslationEntity).pipe(map((r) => r.name));
+
+    this.store.select(selectBooksError).subscribe((error) => {
+      if (error)
+        this._snackBar.open((error as any).message, "Dismiss", {
+          horizontalPosition: "center",
+          verticalPosition: "top",
+        });
+    });
   }
 
   getChapters(book: Book) {
