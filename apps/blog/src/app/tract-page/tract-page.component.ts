64638ade06e16a0d26c29519fbeeb2e45932218d
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-08-07 18:21:43 -0500

    update tracts page

--- apps/blog/src/app/tract-page/tract-page.component.ts
+++ apps/blog/src/app/tract-page/tract-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "blog-tract-page",
   templateUrl: "./tract-page.component.html",
   styleUrl: "./tract-page.component.css",
 })
-export class TractPageComponent {}
+export class TractPageComponent {
+  constructor(private router: Router) {}
+  openLink(link: string) {
+    window.open(link, "_blank");
+  }
+}
