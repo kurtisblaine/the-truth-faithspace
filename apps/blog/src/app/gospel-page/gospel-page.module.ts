20efbbe92a4fec1904a491c71eee20362c475f65
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-08-20 20:38:20 -0500

    add for next time

--- apps/blog/src/app/gospel-page/gospel-page.module.ts
+++ apps/blog/src/app/gospel-page/gospel-page.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { RouterModule, Routes } from "@angular/router";
 import { IvyCarouselModule } from "@degloman/angular-responsive-carousel";
 import { SharedModule } from "../shared/shared.module";
+import { GospelItemComponent } from "./gospel-item/gospel-item.component";
 import { GospelComponent } from "./gospel.component";
 
 const routes: Routes = [
@@ -11,10 +12,21 @@ const routes: Routes = [
     path: "truth",
     component: GospelComponent,
   },
+  {
+    path: "truth/:id",
+    component: GospelItemComponent,
+  },
 ];
 
 @NgModule({
   declarations: [GospelComponent],
-  imports: [IvyCarouselModule, CommonModule, SharedModule, MatButtonModule, RouterModule.forChild(routes)],
+  imports: [
+    IvyCarouselModule,
+    CommonModule,
+    SharedModule,
+    MatButtonModule,
+    RouterModule.forChild(routes),
+    GospelItemComponent,
+  ],
 })
 export class GospelPageModule {}
