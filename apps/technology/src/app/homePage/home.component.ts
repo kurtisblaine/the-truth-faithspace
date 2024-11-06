db6bf5d0739a7fac50877c6a354de568f5b76da8
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-11-06 11:43:29 -0600

    update

--- apps/technology/src/app/homePage/home.component.ts
+++ apps/technology/src/app/homePage/home.component.ts
@@ -1,13 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatDividerModule } from "@angular/material/divider";
 import { fadeInOut } from "../shared/animations/animation";
 @Component({
   selector: "app-home",
-  standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatDividerModule],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
   animations: [fadeInOut],
