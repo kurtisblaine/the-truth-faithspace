5404d937908ff5889c7e3391292f4d13e8c2d58d
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-11-30 14:47:41 -0600

    this works

--- apps/gospel/src/app/gospel-page/gospel-item/gospel-item.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-item/gospel-item.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
-
+import { AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef } from "@angular/core";
+import { TemplateService } from "../template.service";
 @Component({
   selector: "blog-gospel-item",
-  standalone: true,
-  imports: [],
   templateUrl: "./gospel-item.component.html",
   styleUrl: "./gospel-item.component.css",
 })
-export class GospelItemComponent {}
+export class GospelItemComponent implements OnInit, AfterViewInit {
+  public element: HTMLElement;
+
+  @ViewChild("container", { read: ViewContainerRef }) private container: ViewContainerRef;
+
+  constructor(private templateService: TemplateService) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.container.createEmbeddedView(this.templateService.templateRef);
+  }
+}
