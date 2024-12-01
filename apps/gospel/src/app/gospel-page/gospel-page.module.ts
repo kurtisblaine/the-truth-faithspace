2dc29e6ac8442d583278c55431279387cf62699a
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-11-30 20:46:55 -0600

    update to open sections

--- apps/gospel/src/app/gospel-page/gospel-page.module.ts
+++ apps/gospel/src/app/gospel-page/gospel-page.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import { RouterModule, Routes } from "@angular/router";
 import { IvyCarouselModule } from "@degloman/angular-responsive-carousel";
 import { SharedModule } from "../shared/shared.module";
@@ -21,7 +22,14 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [GospelComponent, GospelItemComponent],
-  imports: [IvyCarouselModule, CommonModule, SharedModule, MatButtonModule, RouterModule.forChild(routes)],
+  imports: [
+    IvyCarouselModule,
+    CommonModule,
+    SharedModule,
+    MatButtonModule,
+    RouterModule.forChild(routes),
+    MatCardModule,
+  ],
   providers: [TemplateService],
 })
 export class GospelPageModule {}
