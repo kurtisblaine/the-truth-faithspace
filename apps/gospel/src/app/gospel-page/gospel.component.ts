a17f99459d32fefd51072426b147f34e533d1ac7
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-12-04 17:16:13 -0600

    scrolling navigation

--- apps/gospel/src/app/gospel-page/gospel.component.ts
+++ apps/gospel/src/app/gospel-page/gospel.component.ts
@@ -1,7 +1,17 @@
 import { ViewportScroller } from "@angular/common";
-import { AfterViewInit, Component, HostBinding, OnInit, TemplateRef } from "@angular/core";
-import { Router } from "@angular/router";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 import { fadeInOut } from "shared";
+import { RouterService } from "./router.service";
 import { TemplateService } from "./template.service";
 @Component({
   selector: "blog-gospel",
@@ -12,16 +22,54 @@ import { TemplateService } from "./template.service";
 export class GospelComponent implements AfterViewInit, OnInit {
   @HostBinding("id.scrollProgress") someField = true;
 
+  private sections: ElementRef[] = [];
+  @ViewChild("dangerFutility") dangerFutility: ElementRef;
+  @ViewChild("dangerSin") dangerSin: ElementRef;
+  @ViewChild("callGrace") callGrace: ElementRef;
+  @ViewChild("callFaith") callFaith: ElementRef;
+  @ViewChild("ourResponse") ourResponse: ElementRef;
+  @ViewChild("theHope") hopeTheDay: ElementRef;
+
+  @HostListener("window:scroll", [])
+  onScroll() {
+    if (!this.sections.length) return;
+
+    for (const section of this.sections) {
+      if (this.routerService.isElementInViewport(section.nativeElement)) {
+        const isTheSame = window.location.hash.split("#").pop() == section.nativeElement.id;
+        if (isTheSame) return;
+
+        this.router.navigate([], {
+          fragment: section.nativeElement.id,
+          queryParamsHandling: "merge",
+          onSameUrlNavigation: "ignore",
+        });
+        break;
+      }
+    }
+  }
+
   constructor(
     private viewportScroller: ViewportScroller,
     private router: Router,
-    public templateService: TemplateService
+    public templateService: TemplateService,
+    public routerService: RouterService,
+    public activeRoute: ActivatedRoute
   ) {}
 
   ngOnInit() {}
 
   ngAfterViewInit(): void {
     (window as any).BLB.Tagger.pageInit();
+
+    this.sections = [
+      this.dangerFutility,
+      this.dangerSin,
+      this.callGrace,
+      this.callFaith,
+      this.ourResponse,
+      this.hopeTheDay,
+    ];
   }
 
   public toAnchor(elementId: string): void {
---
+++ apps/gospel/src/app/gospel-page/router.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class RouterService {
+  public isElementInViewport(el: HTMLElement): boolean {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+}
