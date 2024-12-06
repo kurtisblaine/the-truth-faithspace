ec8eb63b997cedb0a26aff4c85236c083bacf2d2
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2024-12-06 17:57:16 -0600

    update

--- apps/gospel/src/app/gospel-page/gospel.component.ts
+++ apps/gospel/src/app/gospel-page/gospel.component.ts
@@ -1,4 +1,3 @@
-import { ViewportScroller } from "@angular/common";
 import { AfterViewInit, Component, HostBinding, OnInit, TemplateRef } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { fadeInOut } from "shared";
@@ -13,35 +12,7 @@ import { TemplateService } from "./template.service";
 export class GospelComponent implements AfterViewInit, OnInit {
   @HostBinding("id.scrollProgress") someField = true;
 
-  // private sections: HTMLElement[] = [];
-  // @ViewChild("dangerFutility") dangerFutility: ElementRef;
-  // @ViewChild("dangerSin")
-  // dangerSin: ElementRef;
-  // @ViewChild("callGrace") callGrace: ElementRef;
-  // @ViewChild("callFaith") callFaith: ElementRef;
-  // @ViewChild("ourResponse") ourResponse: ElementRef;
-  // @ViewChild("theHope") hopeTheDay: ElementRef;
-
-  // @HostListener("window:scroll", [])
-  // onScroll() {
-  //   if (!this.sections.length) return;
-
-  //   for (const section of this.sections) {
-  //     if (this.routerService.isElementInViewport(section)) {
-  //       const isTheSame = window.location.hash.split("#").pop() == section.id;
-  //       if (isTheSame) return;
-
-  //       this.router.navigate([], {
-  //         fragment: section.id,
-  //         queryParamsHandling: "merge",
-  //       });
-  //       break;
-  //     }
-  //   }
-  // }
-
   constructor(
-    private viewportScroller: ViewportScroller,
     private router: Router,
     public templateService: TemplateService,
     public routerService: RouterService,
@@ -51,24 +22,6 @@ export class GospelComponent implements AfterViewInit, OnInit {
   ngOnInit() {}
 
   ngAfterViewInit(): void {
-    const hashes = window.location.hash.split("#");
-    if (hashes.length > 2) {
-      this.viewportScroller.scrollToAnchor(hashes.pop());
-      // this.router.navigate([], {
-      //   fragment: hashes.pop(),
-      //   queryParamsHandling: "merge",
-      // });
-    }
-
-    // this.sections = [
-    //   this.dangerFutility.nativeElement,
-    //   this.dangerSin.nativeElement,
-    //   this.callGrace.nativeElement,
-    //   this.callFaith.nativeElement,
-    //   this.ourResponse.nativeElement,
-    //   this.hopeTheDay.nativeElement,
-    // ];
-
     (window as any).BLB.Tagger.pageInit();
   }
 
