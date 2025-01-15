4548091bc7d900e871b6b5f8f104235c8bcfd9c8
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-01-14 22:04:34 -0600

    ssr on?

--- apps/gospel/src/app/gospel-page/window.service.ts
+++ apps/gospel/src/app/gospel-page/window.service.ts
@@ -1,4 +1,4 @@
-import { isPlatformBrowser } from "@angular/common";
+import { isPlatformBrowser, isPlatformServer } from "@angular/common";
 import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
 
 @Injectable({
@@ -9,5 +9,8 @@ export class WindowService {
     return isPlatformBrowser(this.platformId) ? window : undefined;
   }
 
-  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    console.log("isPlatformBrowser" + isPlatformBrowser(this.platformId));
+    console.log("isPlatformServer" + isPlatformServer(this.platformId));
+  }
 }
