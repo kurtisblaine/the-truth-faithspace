240dae5b47aad1493fa2c0a1861b264d15c663cb
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-04-20 19:58:12 -0500

    update email page to have container

--- apps/gospel/src/app/email-page/email-page.module.ts
+++ apps/gospel/src/app/email-page/email-page.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
@@ -27,6 +28,7 @@ const routes: Routes = [
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule,
   ],
   providers: [ContactService],
 })
