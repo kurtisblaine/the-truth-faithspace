9a6c88d7555a8225028abc1f8ad43a3e489f8fc8
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-04-24 15:24:57 -0500

    updates

--- libs/src/lib/shared/components/narrator/speech.service.ts
+++ libs/src/lib/shared/components/narrator/speech.service.ts
@@ -1,32 +1,57 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: "root",
+  providedIn: "any",
 })
 export class SpeechService {
   private speechSynthesis: SpeechSynthesis;
+  private isPaused = new BehaviorSubject<boolean>(false);
+  public isPaused$ = this.isPaused.asObservable();
+
+  private isPending = new BehaviorSubject<boolean>(false);
+  public isPending$ = this.isPending.asObservable();
+
+  private isSpeaking = new BehaviorSubject<boolean>(false);
+  public isSpeaking$ = this.isSpeaking.asObservable();
+
+  private state = new BehaviorSubject<string>("stopped");
+  public state$ = this.state.asObservable();
 
   constructor() {
     this.speechSynthesis = window.speechSynthesis;
+    this.state.next("stopped");
   }
 
   speak(text: string) {
+    this.cancel(); //clear out the previous one...
+    const voices = this.speechSynthesis.getVoices();
+    console.log(voices);
     const utterance = new SpeechSynthesisUtterance(text);
-    utterance.volume = 10;
     utterance.lang = "eng";
     // utterance.voice = new SpeechSynthesisVoice();
     this.speechSynthesis.speak(utterance);
+    this.isSpeaking.next(true);
+    this.state.next("speaking");
   }
 
   pause() {
     this.speechSynthesis.pause();
+    this.isPaused.next(true);
+    this.isSpeaking.next(false);
+    this.state.next("paused");
   }
 
   resume() {
     this.speechSynthesis.resume();
+    this.isSpeaking.next(true);
+    this.state.next("speaking");
   }
 
   cancel() {
     this.speechSynthesis.cancel();
+    this.isPaused.next(false);
+    this.isSpeaking.next(false);
+    this.state.next("stopped");
   }
 }
