4fd98655fd909e8f05ef4f4f3a507191492a1b8d
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-04-24 19:28:15 -0500

    button updates

--- libs/src/lib/shared/shared.module.ts
+++ libs/src/lib/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
@@ -24,6 +25,8 @@ import { TooltipDirective } from "./directives/tooltip.directive";
     FontAwesomeModule,
     NgxEditorModule.forRoot(),
     MatButtonModule,
+    MatButtonToggleModule,
+    FormsModule,
   ],
   exports: [TooltipDirective, TextEditorComponent, NarratorComponent],
 })
