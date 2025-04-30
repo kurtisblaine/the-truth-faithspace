a2d4ef5ca737f4899f0ebf5a98b33cc1fafe8f6b
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-04-30 15:09:33 -0500

    pollyfill for other apps

--- apps/gospel/src/polyfills.ts
+++ apps/gospel/src/polyfills.ts
@@ -45,6 +45,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import "web-speech-cognitive-services";
 import "zone.js"; // Included with Angular CLI.
 
 /***************************************************************************************************
