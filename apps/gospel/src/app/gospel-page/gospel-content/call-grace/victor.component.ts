a47c2f2c43bc03ed99cb04c2f5fc361c559a2d54
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-05-14 14:03:37 -0500

    u

--- apps/gospel/src/app/gospel-page/gospel-content/call-grace/victor.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-content/call-grace/victor.component.ts
@@ -14,60 +14,59 @@ import { SharedModule } from "../../../shared/shared.module";
       tricked by the one he loved, the one he ate bread with, and she shaved off the seven locks of his head, and the
       LORD, who gave him his strength, left him; then he was subdued by his enemies, they gouged out his eyes and put
       him into prison, where his hair began to grow again. Then the time came when the Philistines, merry with wine,
-      called for Samson to make a spectacle of him since he was their enemy, but Samson prayed to the Lord,
+      called for Samson to make a spectacle of him since he was their enemy; but Samson prayed to the Lord,
       <blog-reference-tooltip [reference]="'Judges 16:28'">
         O Lord God, remember me, I pray! Strengthen me, I pray, just this once, O God, that I may with one blow take
         vengeance on the Philistines for my two eyes!
       </blog-reference-tooltip>
-      ” And Samson took hold of the two middle pillars which supported the temple, and he braced himself against them,
+      ”And Samson took hold of the two middle pillars which supported the temple, and he braced himself against them,
       one on his right and the other on his left." (Judges 16:29) "Then Samson said, “Let me die with the Philistines!”
       And he pushed with all his might, and the temple fell on the lords and all the people who were in it. So the dead
       that he killed at his death were more than he had killed in his life." (Judges 16:30) So Samson made a spectacle
       of them in that he destroyed them in his death. <br /><br />
 
       And Jesus, having removed many demons from people who were oppressed, subduing them, freeing them who were held
-      captive by satan, was sent by God to be a judge, to be a deliverer; at the end of His life, He was betrayed by
-      Judas, His beloved friend, and turned over to the Gentiles for mockery; just as it was written in the prophet's
-      Psalm,
+      captive by satan, was sent by God to be Judge, to be Deliverer; at the end of His life, He was betrayed by Judas,
+      His beloved friend, and turned over to the Gentiles for mockery; just as it was written in the prophet's Psalm,
       <blog-reference-tooltip [reference]="'Psalm 41:9'">
         Even my close friend in whom I trusted, Who ate my bread, Has lifted up his heel against me.
       </blog-reference-tooltip>
-      He was crucified, and made a public spectacle of by the Romans as they called a legion of soldiers to formation
-      and mocked Him publically and beat Him; then, making a spectacle of Him through the cross, lifted Him up for all
-      to see; the written charge against Him, "JESUS THE NAZARENE, KING OF THE JEWS". But it was in His death, that He
+      He was crucified, and made a public spectacle of by the Romans; they called a legion of soldiers to formation and
+      mocked Him publically and beat Him; then, making a spectacle of Him through the cross, lifted Him up for all to
+      see; the written charge against Him, "JESUS THE NAZARENE, KING OF THE JEWS". But it was in His death, that He
       disarmed more of the enemies of the rulers and principalities of this world than He did in His life, defeating
-      them and rendering them powerless. For the power of a lion is in its mouth, for it has sharp and powerful teeth,
-      and the power of satan is in his mouth, his lies; but we have been set free by the Truth, namely Christ Jesus
-      crucified for us, received through faith in Him, and in Him there is no condemnation; for if we walk according to
-      the Spirit, we are no longer under the law, but under grace; under Him and in Him, there is no sin; so while we
-      are in Him, we are free from the power of sin, for where the Spirit of the Lord is there is freedom. While the
-      rulers of this world thought they were destroying Him, they were being destroyed; while they were mocking Him,
-      they were being mocked; while they put Him on display, they were put on public display. And later, as prophesied,
-      the accusations of the enemy will cease; as the Revelation says,
+      them and rendering them powerless through the cross. For the power of a lion is in its mouth, for it has sharp and
+      powerful teeth, and the power of satan is in his mouth, namely his lies; but we have been set free by the Truth,
+      namely Christ Jesus crucified for us, received through faith in Him, and in Him there is no condemnation; for if
+      we walk according to the Spirit, we are no longer under the law, but under grace; under Him and in Him, there is
+      no sin; so while in Him, we are free from the power of sin, for where the Spirit of the Lord is there is freedom.
+      While the rulers of this world thought they were destroying Him, they were being destroyed; while they were
+      mocking Him, they were being mocked; while they put Him on display, they were put on display. And later, as
+      prophesied, the accusations of the enemy will cease; as the Revelation says,
       <blog-reference-tooltip [reference]="'Revelation 12:10-12'">
         for the accuser of our brethren has been thrown down, he who accuses them before our God day and night.
       </blog-reference-tooltip>
-      And the prophecy also describes satan's rule ending, and Christ's kingdom coming. For Jesus fought the battle for
-      us and we enter into His victory; for our God is for us, doing on our behalf, fighting for us. Just as the
-      Israelites entered into the Promised Land by the mighty hand of God, so it is with us: we enter in by His powerful
-      arm, following and obeying Him. Other gods require that you do something for them: stand them up, feed them, dust
-      them off, but they are no gods at all; the one, and only God, does on our behalf, waging war for us and defeating
-      our foes. He defeated them through the cross and we enter into His victory through faith. If we dwell in lies,
-      being deceived or deceiving ourselves, we are under oppression, but the truth makes us free.
+      And the prophecy also describes satan's rule ending, and Christ's kingdom coming - and His reign is eternal in an
+      eternal kingdom. For Jesus fought the battle for us and we enter into His victory; for our God is for us, doing on
+      our behalf, fighting for us. Just as the Israelites entered into the Promised Land by the mighty hand of God, so
+      it is with us: we enter in by His powerful arm, following and obeying Him. Other gods require that you do
+      something for them: stand them up, feed them, dust them off, but they are no gods at all; the one, and only God,
+      does on our behalf, waging war for us and defeating our foes. He defeated them through the cross and we enter into
+      His victory through faith. If we dwell in lies, being deceived or deceiving ourselves, we are under oppression,
+      but the truth makes us free.
       <!-- TODO: put the victorious link here -->
       <br /><br />
 
-      Likewise, in a different testimony, Haman was an enemy of the Jews who sought to kill the innocent Mordecai and
-      with him, the Jewish nation. He payed money to the king to establish a decree: on a certain day, all Jews would be
-      killed. Furthermore, he had set up a pole by his home to impale Mordecai on it; and what was preventing him? He
-      needed the permission of the king. Before all this, Mordecai saved the same king from being killed by spies; while
-      Haman went to go ask the king for permission to impale Mordecai, the king discovered from the book of the
-      Chronicles that Mordecai saved the king in the past; so the king asked Haman while he came in to ask for
-      Mordecai's death, "What should be done to the one whom the king is pleased", and Haman, thinking the good was for
-      himself, gave a favorable answer. The king then told him to do all which he said unto Mordecai, the Jew, which was
-      Haman's enemy.
+      Likewise, in a different testimony (in the book of Ester), Haman, an enemy of the Jews, sought to kill the
+      innocent Mordecai and with him, the Jewish nation. He payed money to the king to establish a decree: on a certain
+      day, all Jews would be killed. Furthermore, he had set up a pole by his home to impale Mordecai on it; and what
+      was preventing him? He needed the permission of the king. But Mordecai was righteous and innocent: he saved the
+      same king from being killed by spies. So while Haman went to go ask the king for permission to impale Mordecai,
+      the king discovered from the book of the Chronicles that Mordecai saved him; so the king asked Haman, "What should
+      be done to the one whom the king is pleased?"; and Haman, thinking the good was for himself, gave a favorable
+      answer. The king then told him to do all he said to Mordecai, the Jew, which was Haman's enemy.
       <b>So the good Haman intended for himself went to Mordecai, his enemy.</b>
-      And later at a banquet, Ester, the Queen, who was a Jew and the niece of Mordecai, spoke of Haman's decree to kill
+      And later at a banquet, Ester, the Queen, who was a Jew and the niece to Mordecai, spoke of Haman's decree to kill
       the Jews which was approved by the king in ignorance, for he did not know the Queen was a Jew, for she kept her
       nationality hidden. And the king was furious because Haman sought to kill the Queen and he had Haman instead
       killed; he was impalled on the pole which he set up for Mordecai, for it was told him of Haman's plan.
