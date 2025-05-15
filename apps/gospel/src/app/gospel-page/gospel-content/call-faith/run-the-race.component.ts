c29a3b4342f4d26fe81cc770eff094d4b6959c99
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-05-15 17:21:23 -0500

    updates

--- apps/gospel/src/app/gospel-page/gospel-content/call-faith/run-the-race.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-content/call-faith/run-the-race.component.ts
@@ -7,25 +7,42 @@ import { SharedModule } from "../../../shared/shared.module";
   imports: [CommonModule, SharedModule],
   template: `<ng-container *ngTemplateOutlet="theRaceOfFaith"></ng-container>
     <ng-template #theRaceOfFaith>
-      Run the race of faith! The map is God's Word, the runner's shoes the Gospel of peace, the destination is
-      heavenward, the prize eternal life. How we start is how we end: from faith to faith;
-      <blog-reference-tooltip [reference]="'1 Corinthians 9:24-27'">
-        Do you not know that in a race all the runners run, but <b>only one</b> receives the prize? So run that you may
-        obtain it. Every athlete exercises self-control in all things. They do it to receive a perishable wreath, but we
-        an imperishable. So I do not run aimlessly; I do not box as one beating the air. But I discipline my body and
-        keep it under control, lest after preaching to others I myself should be disqualified.
+      Run the race of faith! The map is God's Word, the runner's shoes is the Gospel of peace, the destination is
+      heavenward, the prize is eternal life. And how we start is how we end: from faith to faith; this is no triathalon,
+      where a man starts on his feet and ends up on a bike, and then in the sea - no! as the apostle Paul says, “Are you
+      so foolish? Having begun by the Spirit, are you now being perfected by the flesh?” (Galatians 3:3) And, "You were
+      running a good race. Who cut in on you to keep you from obeying the truth?" (Galatians 5:7) And concerning the
+      Gospel he says, “For in it the righteousness of God is revealed from faith to faith; as it is written, "BUT THE
+      RIGHTEOUS man SHALL LIVE BY FAITH."” (Romans 1:17) So it is: we start by faith and end by faith, from start to
+      finish, it is by faith in the Son of God who loved us. Everything we do stems from the root; if the branches bear
+      fruit, it is because of the root; for "It is not you who supports the root, but the root supports you.” (Romans
+      11:18) You are only a branch. We are supported by the foundation of roots; we cannot stand without. For Christ
+      makes us to stand and to walk uprightly, so when He makes our paths straight - run! The world we live in wants to
+      always draw us away from Christ to be busy and rushing in the world; but where are we rushing to? Out of the
+      context of a race, we are running in vain, rushing to our death; and what's more vain than death? Do not run in
+      vain; as it says,
+      <blog-reference-tooltip [reference]="'Galatians 2:2'">
+        I wanted to be sure I was not running and had not been running my race in vain.
       </blog-reference-tooltip>
-      And,
-      <blog-reference-tooltip [reference]="'2 Timothy 2:5, 2 Timothy 4:8'">
-        Similarly, anyone who competes as an athlete does not receive the victorʼs crown except by competing according
-        to the rules. (...) I have finished the race, I have kept the faith. Now there is in store for me the crown of
-        righteousness
+      Running implies you have a place to go and quickly; and in the context of a race: the place to go is the finish
+      line. It also implies hard work, for running is by no means walking; although our faith is also called a walk;
+      <blog-reference-tooltip [reference]="'2 Corinthians 5:7'">
+        For we <b>walk</b> by faith, not by sight.
       </blog-reference-tooltip>
-      And,
-      <blog-reference-tooltip [reference]="'Galatians 2:2, Galatians 5:7'">
-        I wanted to be sure I was not running and had not been running my race in vain. You were running a good race.
-        (...) Who cut in on you to keep you from obeying the truth? </blog-reference-tooltip
-      ><blog-link
+      But running is likened to diligence;
+      <blog-reference-tooltip [reference]="'Romans 12:10,11'">
+        <b>not lagging behind in diligence</b>, fervent in spirit, serving the Lord;
+      </blog-reference-tooltip>
+      Therefore, run in zeal, zealous for good works, according to the true knowledge of Christ. For what is the fruit
+      of the world? As the Lord says,
+      <blog-reference-tooltip [reference]="'Matthew 13:22'" [isJesusWord]="true">
+        As for what was sown among thorns, this is the one who hears the word, but the cares of the world and the
+        deceitfulness of riches choke the word, and it proves unfruitful.
+      </blog-reference-tooltip>
+      But if we run the race of faith in Jesus Christ, we run to life eternal. So run! And to win! The race of faith is
+      also the good fight; we fight with words of truth told by the Spirit, who is our Sword, with words seasoned with
+      salt, preserving the truth of God's grace.
+      <blog-link
         [title]="'See more about the Race of Faith'"
         [link]="'https://thelightof.life/#/draw/runtherace.webp'"
       ></blog-link
@@ -33,25 +50,33 @@ import { SharedModule } from "../../../shared/shared.module";
 
       No one runs a race for a prize without first training, and they who run, run to win; they strip off every weight
       that slows them down, for the prize; they learn the rules and know exactly what they can and cannot do; runners
-      are disciplined in what goes into their body, and they have strict schedules for training; no one runs the race to
-      get second place, but first place, otherwise all the training and running would have been in vain. They also learn
-      endurance through tribulations. This is used as an illustration of the race we run: the true race by living a life
-      of faith in Jesus Christ. As the writer says,
+      are disciplined in what goes into their body, and they have strict schedules for training; they find the right
+      shoes for running long distances. No one runs a race to get second place, but they run to get first place;
+      otherwise all the training and running would have been in vain.
+      <blog-reference-tooltip [reference]="'1 Corinthians 9:25-27'">
+        Every athlete exercises self-control in all things. They do it to receive a perishable wreath, but we an
+        imperishable. So I do not run aimlessly; I do not box as one beating the air. But I discipline my body and keep
+        it under control, lest after preaching to others I myself should be disqualified.
+      </blog-reference-tooltip>
+      They also learn endurance through suffering and trials. And this is the race we run: the true race by living a
+      life of faith in Jesus Christ; as the writer says,
       <blog-reference-tooltip [reference]="'Hebrews 12:2'">
         Let us throw off everything that hinders and the sin that so easily entangles. And let us run with perseverance
         the race marked out for us, fixing our eyes on Jesus, the pioneer and perfecter of faith.
       </blog-reference-tooltip>
-      Disciple means "disciplined one" and we are called disciples of Christ; and self-discipline is a fruit of the Holy
-      Spirit. Any other discipline that does not come from the Holy Spirit is not truly self-discipline. When I was in
-      the military, they told me I would have self-discipline, but what I found was the military disciplined me and my
-      discipline did not come from myself; therefore, it was not self-discipline. In fact, when I got out of the
-      military, I did all the things I couldn't do while in the military, but to greater degree. For I desired to do
-      them, but I couldn't - and this is yet another form of slavery. I didn't do according to my desires, but I willed
-      to do so. Is this any different from willing to do good, but not having the ability? In both cases, the will is
-      present but the doing is not - still, sin is slavery regardless. So I found that the discipline they tried to
-      inflict upon me, actually made me worse; for the things they tried to inflict upon me according to the strength of
-      human flesh, had a reverse effect upon me - it strengthened my sinful flesh. But thank God, He gives us the Holy
-      Spirit that we may overcome ourselves! As the warning to Abel says,
+      As it says, "fixing our eyes on Jesus", but Jesus is unseen, so it is, we fix our eyes on Jesus by faith; and this
+      is the race we run: the race of faith. For disciple means "disciplined one", and we are called disciples of
+      Christ; self-discipline is a fruit of the Holy Spirit, and discipline is needed for running the race. Any
+      discipline that does not come from the Holy Spirit is not truly self-discipline. For when I was in the military,
+      they told me I would have self-discipline, but what I found was the military disciplined me, and my discipline did
+      not come from myself; therefore, it was not self-discipline. In fact, when I got out of the military, I did all
+      the things I couldn't do while I was in the military, but to a greater degree. For I desired to do them, but I
+      couldn't - and this is yet another form of slavery. I didn't do according to my desires, but I willed to do so. Is
+      this any different from willing to do good, but not having the ability? In both cases, the will is present but the
+      doing is not - but still, sin is slavery regardless. So I found that the discipline they tried to inflict upon me,
+      actually made me worse; for the things they tried to inflict upon me which was according to the strength of human
+      flesh, had a reverse effect upon me - it strengthened my sinful flesh. But thank God, He gives us the Holy Spirit
+      that we may overcome ourselves, that is, our flesh! As the warning to Abel says,
       <blog-reference-tooltip [reference]="'Genesis 4:7'">
         If you do well, will not your countenance be lifted up? And if you do not do well, sin is crouching at the door;
         and its desire is for you, <b>but you must master it.</b>
@@ -62,39 +87,43 @@ import { SharedModule } from "../../../shared/shared.module";
       ></blog-link
       ><br /><br />
 
-      We run the race of faith not for a perishable crown, but for an indestructible crown, and that of life. Running
-      implies you have a place to go and quickly; and in the context of a race: the finish line. It also implies hard
-      work, for running is by no means walking; although our faith is also called a walk, saying,
-      <blog-reference-tooltip [reference]="'2 Corinthians 5:7'">
-        For we <b>walk</b> by faith, not by sight.
+      We run the race of faith not for a perishable crown, but for an indestructible crown, and that of life; as Paul
+      says, "Do you not know that in a race all the runners run, but <b>only one</b> receives the prize? So run that you
+      may obtain it." (1 Corinthians 9:24) And at the end of his life says,
+      <blog-reference-tooltip [reference]="'2 Timothy 4:8'">
+        I have finished the race, I have kept the faith. Now there is in store for me the crown of righteousness.
       </blog-reference-tooltip>
-      Running is diligence, as it says,
-      <blog-reference-tooltip [reference]="'Romans 12:10,11'">
-        Be devoted to one another in brotherly love; give preference to one another in honor;
-        <b>not lagging behind in diligence</b>, fervent in spirit, serving the Lord;
-      </blog-reference-tooltip>
-      So when Christ makes our paths straight - run! The world we live in wants to always draw us away from Christ to be
-      busy and always rushing in the world; but where are we rushing to? Out of the context of a race, we are running in
-      vain, rushing to our death; and what's more vain than death? The Lord says,
-      <blog-reference-tooltip [reference]="'Matthew 13:22'" [isJesusWord]="true">
-        As for what was sown among thorns, this is the one who hears the word, but the cares of the world and the
-        deceitfulness of riches choke the word, and it proves unfruitful.
-      </blog-reference-tooltip>
-      But if we run the race of faith in Jesus Christ, we run to life eternal. So run! And to win! The race of faith is
-      also the good fight; we fight with words of truth by the Spirit, who is our Sword, with words seasoned with salt,
-      preserving the truth of God's grace and breaking down every stronghold built up by satan in the hearts of men,
-      bringing them into subjection to the truth, so that they may experience salvation. Many will try to enter the
-      kingdom, but will be forbidden; many run the race, but only one wins the prize. We must know for certain that not
-      everyone will enter by mere will power; as Christ says,
+      Many will try to enter the kingdom, but will be forbidden; many run the race, but only one wins the prize. We must
+      know for certain that not everyone will enter by their own mere will power; but as Christ says,
       <blog-reference-tooltip [reference]="'Matthew 7:21'" [isJesusWord]="true">
         Not everyone who says to me, ‘Lord, Lord,’ will enter the kingdom of heaven, but the one who does the will of my
         Father who is in heaven.
       </blog-reference-tooltip>
-      And Paul saying,
+      "Similarly, anyone who competes as an athlete does not receive the victorʼs crown except by competing according to
+      the rules." (2 Timothy 2:5) So it is by doing the will of the Father; and it is the Father who works in us both
+      the will and the work; as it says, “for it is God who is at work in you, both to will and to work for His good
+      pleasure.” (Philippians 2:13) And furthermore, Paul says,
       <blog-reference-tooltip [reference]="'Romans 9:16'"
         >So then it does not depend on the man who wills or the man who runs, but on God who has
         mercy.</blog-reference-tooltip
       >
+      So human strength does not accomplish God's will, but the work of faith accompanied by God's grace. Again, the
+      race of faith is not physical; all physical effort produces sweat, and sweat is given us as a result of the curse.
+      For God cursed the ground for our sakes, and by the sweat of our brow, we eat its bread. But what does the law say
+      concerning the land? “Six years you shall sow your field, and six years you shall prune your vineyard and gather
+      in its crop, but during the seventh year the land shall have a sabbath rest, a sabbath to the LORD; you shall not
+      sow your field nor prune your vineyard.” (Leviticus 25:3-4) The man who wills and runs says, "What are we going to
+      eat on the seventh year if we do not sow or gather in our crops?" (Leviticus 25:20) But God says, “I will so order
+      My blessing for you in the sixth year that it will bring forth the crop for three years. When you are sowing the
+      eighth year, you can still eat old things from the crop, eating the old until the ninth year when its crop comes
+      in.” (Leviticus 25:21-22) So the men were to rest in the Seventh year, and God worked a blessing for three years.
+      And when the men worked the land by the sweat of their own brow, it says, “You have sown much, but harvest little;
+      you eat, but there is not enough to be satisfied; you drink, but there is not enough to become drunk; you put on
+      clothing, but no one is warm enough; and he who earns, earns wages to put into a purse with holes." (Haggai 1:6)
+      But when the Word of faith came through Haggai, the prophet: “Then they obeyed the voice of the LORD their God and
+      the words of Haggai the prophet, as the LORD their God had sent him. And the people showed reverence for the
+      LORD.” (Haggai 1:12) And the people worked out their faith in reverence, subjecting themselves to the Word of the
+      LORD; and the LORD their God was with them.
     </ng-template>`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
