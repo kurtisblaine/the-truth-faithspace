fc705ac75d7c51a19c5875ee74e29de9c71a2336
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-05-15 17:21:30 -0500

    race

--- apps/gospel/src/app/gospel-page/gospel-content/call-faith/run-the-race.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-content/call-faith/run-the-race.component.ts
@@ -9,18 +9,18 @@ import { SharedModule } from "../../../shared/shared.module";
     <ng-template #theRaceOfFaith>
       Run the race of faith! The map is God's Word, the runner's shoes is the Gospel of peace, the destination is
       heavenward, the prize is eternal life. And how we start is how we end: from faith to faith; this is no triathalon,
-      where a man starts on his feet and ends up on a bike, and then in the sea - no! as the apostle Paul says, “Are you
+      where a man starts on his feet and ends up on a bike, and then in the sea - no! As the apostle Paul says, “Are you
       so foolish? Having begun by the Spirit, are you now being perfected by the flesh?” (Galatians 3:3) And, "You were
       running a good race. Who cut in on you to keep you from obeying the truth?" (Galatians 5:7) And concerning the
-      Gospel he says, “For in it the righteousness of God is revealed from faith to faith; as it is written, "BUT THE
-      RIGHTEOUS man SHALL LIVE BY FAITH."” (Romans 1:17) So it is: we start by faith and end by faith, from start to
-      finish, it is by faith in the Son of God who loved us. Everything we do stems from the root; if the branches bear
-      fruit, it is because of the root; for "It is not you who supports the root, but the root supports you.” (Romans
-      11:18) You are only a branch. We are supported by the foundation of roots; we cannot stand without. For Christ
-      makes us to stand and to walk uprightly, so when He makes our paths straight - run! The world we live in wants to
-      always draw us away from Christ to be busy and rushing in the world; but where are we rushing to? Out of the
-      context of a race, we are running in vain, rushing to our death; and what's more vain than death? Do not run in
-      vain; as it says,
+      Gospel he says, 'For in it the righteousness of God is revealed from faith to faith; as it is written, "BUT THE
+      RIGHTEOUS man SHALL LIVE BY FAITH.'” (Romans 1:17) So it is: we start by faith and end by faith, from start to
+      finish, it is by faith in the Son of God who loves us. Everything we do stems from the root; if the branches bear
+      fruit, it is because of the root; for, "It is not you who supports the root, but the root supports you.” (Romans
+      11:18) You are only a branch; we are being supported by the foundation of roots; we cannot stand without. For
+      Christ makes us to stand and to walk uprightly, so when He makes our paths straight - run! The world wants to draw
+      us away from Christ to be busy and rushing in the world; but where are we rushing to? Out of the context of a
+      race, we are running in vain, rushing to our death; and what's more vain than death? Do not run in vain; as Pauls
+      says,
       <blog-reference-tooltip [reference]="'Galatians 2:2'">
         I wanted to be sure I was not running and had not been running my race in vain.
       </blog-reference-tooltip>
@@ -30,11 +30,12 @@ import { SharedModule } from "../../../shared/shared.module";
         For we <b>walk</b> by faith, not by sight.
       </blog-reference-tooltip>
       But running is likened to diligence;
-      <blog-reference-tooltip [reference]="'Romans 12:10,11'">
-        <b>not lagging behind in diligence</b>, fervent in spirit, serving the Lord;
+      <blog-reference-tooltip [reference]="'Romans 12:11'">
+        not lagging behind in diligence, fervent in spirit, serving the Lord;
       </blog-reference-tooltip>
-      Therefore, run in zeal, zealous for good works, according to the true knowledge of Christ. For what is the fruit
-      of the world? As the Lord says,
+      For this is the work of the Lord: the "work of faith and labor of love." (1 Thessalonians 1:3) Therefore, run in
+      zeal for good works, according to the true knowledge of Christ. For what is the fruit we have in this world? As
+      the Lord says,
       <blog-reference-tooltip [reference]="'Matthew 13:22'" [isJesusWord]="true">
         As for what was sown among thorns, this is the one who hears the word, but the cares of the world and the
         deceitfulness of riches choke the word, and it proves unfruitful.
@@ -48,18 +49,18 @@ import { SharedModule } from "../../../shared/shared.module";
       ></blog-link
       ><br /><br />
 
-      No one runs a race for a prize without first training, and they who run, run to win; they strip off every weight
-      that slows them down, for the prize; they learn the rules and know exactly what they can and cannot do; runners
-      are disciplined in what goes into their body, and they have strict schedules for training; they find the right
-      shoes for running long distances. No one runs a race to get second place, but they run to get first place;
-      otherwise all the training and running would have been in vain.
+      No one runs a race for a prize without first training, and they who run, certainly run to win; they strip off
+      every weight that slows them down, for the prize; they learn the rules and know exactly what they can and cannot
+      do; runners are disciplined in what goes into their body, and they have strict schedules for training; and they
+      find the perfect fitting shoes for running long distances. No one runs a race to get second place, but they run to
+      get first place; otherwise all the training and running would be in vain.
       <blog-reference-tooltip [reference]="'1 Corinthians 9:25-27'">
         Every athlete exercises self-control in all things. They do it to receive a perishable wreath, but we an
         imperishable. So I do not run aimlessly; I do not box as one beating the air. But I discipline my body and keep
         it under control, lest after preaching to others I myself should be disqualified.
       </blog-reference-tooltip>
       They also learn endurance through suffering and trials. And this is the race we run: the true race by living a
-      life of faith in Jesus Christ; as the writer says,
+      life of faith in Jesus Christ; as the writer of Hebrews says,
       <blog-reference-tooltip [reference]="'Hebrews 12:2'">
         Let us throw off everything that hinders and the sin that so easily entangles. And let us run with perseverance
         the race marked out for us, fixing our eyes on Jesus, the pioneer and perfecter of faith.
@@ -70,13 +71,13 @@ import { SharedModule } from "../../../shared/shared.module";
       discipline that does not come from the Holy Spirit is not truly self-discipline. For when I was in the military,
       they told me I would have self-discipline, but what I found was the military disciplined me, and my discipline did
       not come from myself; therefore, it was not self-discipline. In fact, when I got out of the military, I did all
-      the things I couldn't do while I was in the military, but to a greater degree. For I desired to do them, but I
+      the things I could not do while I was in the military, but to a greater degree. For I desired to do them, but I
       couldn't - and this is yet another form of slavery. I didn't do according to my desires, but I willed to do so. Is
       this any different from willing to do good, but not having the ability? In both cases, the will is present but the
       doing is not - but still, sin is slavery regardless. So I found that the discipline they tried to inflict upon me,
       actually made me worse; for the things they tried to inflict upon me which was according to the strength of human
-      flesh, had a reverse effect upon me - it strengthened my sinful flesh. But thank God, He gives us the Holy Spirit
-      that we may overcome ourselves, that is, our flesh! As the warning to Abel says,
+      flesh, had a reverse effect upon me - for it strengthened my sinful flesh. But thank God, He gives us the Holy
+      Spirit that we may overcome ourselves, that is, our flesh! As the warning to Abel says,
       <blog-reference-tooltip [reference]="'Genesis 4:7'">
         If you do well, will not your countenance be lifted up? And if you do not do well, sin is crouching at the door;
         and its desire is for you, <b>but you must master it.</b>
@@ -93,37 +94,41 @@ import { SharedModule } from "../../../shared/shared.module";
       <blog-reference-tooltip [reference]="'2 Timothy 4:8'">
         I have finished the race, I have kept the faith. Now there is in store for me the crown of righteousness.
       </blog-reference-tooltip>
-      Many will try to enter the kingdom, but will be forbidden; many run the race, but only one wins the prize. We must
-      know for certain that not everyone will enter by their own mere will power; but as Christ says,
+      Many will try to enter the kingdom, but will be forbidden; many will run the race, but only one wins the prize. We
+      must know for certain that not everyone will enter by their own mere will power; as Christ says,
       <blog-reference-tooltip [reference]="'Matthew 7:21'" [isJesusWord]="true">
         Not everyone who says to me, ‘Lord, Lord,’ will enter the kingdom of heaven, but the one who does the will of my
         Father who is in heaven.
       </blog-reference-tooltip>
       "Similarly, anyone who competes as an athlete does not receive the victorʼs crown except by competing according to
       the rules." (2 Timothy 2:5) So it is by doing the will of the Father; and it is the Father who works in us both
-      the will and the work; as it says, “for it is God who is at work in you, both to will and to work for His good
-      pleasure.” (Philippians 2:13) And furthermore, Paul says,
+      the will and the working of the will; as it says, “for it is God who is at work in you, both to will and to work
+      for His good pleasure.” (Philippians 2:13) And furthermore, Paul says,
       <blog-reference-tooltip [reference]="'Romans 9:16'"
         >So then it does not depend on the man who wills or the man who runs, but on God who has
         mercy.</blog-reference-tooltip
       >
       So human strength does not accomplish God's will, but the work of faith accompanied by God's grace. Again, the
-      race of faith is not physical; all physical effort produces sweat, and sweat is given us as a result of the curse.
-      For God cursed the ground for our sakes, and by the sweat of our brow, we eat its bread. But what does the law say
+      race of faith is not physical; all physical effort produces sweat, and sweat is a result of the curse. For God
+      cursed the ground for our sakes, and by the sweat of our brow, we eat its bread. But what does the law say
       concerning the land? “Six years you shall sow your field, and six years you shall prune your vineyard and gather
       in its crop, but during the seventh year the land shall have a sabbath rest, a sabbath to the LORD; you shall not
       sow your field nor prune your vineyard.” (Leviticus 25:3-4) The man who wills and runs says, "What are we going to
       eat on the seventh year if we do not sow or gather in our crops?" (Leviticus 25:20) But God says, “I will so order
       My blessing for you in the sixth year that it will bring forth the crop for three years. When you are sowing the
       eighth year, you can still eat old things from the crop, eating the old until the ninth year when its crop comes
-      in.” (Leviticus 25:21-22) So the men were to rest in the Seventh year, and God worked a blessing for three years.
-      And when the men worked the land by the sweat of their own brow, it says, “You have sown much, but harvest little;
-      you eat, but there is not enough to be satisfied; you drink, but there is not enough to become drunk; you put on
-      clothing, but no one is warm enough; and he who earns, earns wages to put into a purse with holes." (Haggai 1:6)
-      But when the Word of faith came through Haggai, the prophet: “Then they obeyed the voice of the LORD their God and
-      the words of Haggai the prophet, as the LORD their God had sent him. And the people showed reverence for the
-      LORD.” (Haggai 1:12) And the people worked out their faith in reverence, subjecting themselves to the Word of the
-      LORD; and the LORD their God was with them.
+      in.” (Leviticus 25:21-22) So the men were to rest in the Seventh year, and God worked a blessing for them for
+      three years thereafter. In the times of Haggai and Zechariah, the prophets, during the rebuilding of the temple,
+      the people worked the land by the sweat of their own brow; and the fruit of their labors were eaten up, but not by
+      them; as it says, “You have sown much, but harvest little; you eat, but there is not enough to be satisfied; you
+      drink, but there is not enough to become drunk; you put on clothing, but no one is warm enough; and he who earns,
+      earns wages to put into a purse with holes." (Haggai 1:6) But when the Word of faith came through Haggai, the
+      prophet, they believed; as it says, “Then they obeyed the voice of the LORD their God and the words of Haggai the
+      prophet, as the LORD their God had sent him. And the people showed reverence for the LORD.” (Haggai 1:12) And the
+      LORD stirred up their spirits; and the LORD their God was with them. They went from sowing and harvesting to
+      rebuilding the temple of the LORD. They worked the work of faith, and ran the race of faith; and by whose
+      strength? As it is written, "'Not by might nor by power, but by My Spirit,' says the LORD of hosts.” (Zechariah
+      4:6) And, "Did you receive the Spirit by the works of the Law, or by hearing with faith?” (Galatians 3:2)
     </ng-template>`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
