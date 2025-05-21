081fc640b05af6d5e0942023c0b04d100fd189fc
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-05-21 15:10:05 -0500

    update new heart text

--- apps/gospel/src/app/gospel-page/gospel-content/call-faith/new-heart.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-content/call-faith/new-heart.component.ts
@@ -14,55 +14,69 @@ import { SharedModule } from "../../../shared/shared.module";
         stumbling stone. As it is written: “See, I lay in Zion a stone that causes people to stumble and a rock that
         makes them fall, and the one who believes in him will never be put to shame.”
       </blog-reference-tooltip>
-      It's an offense to say: those who work hard to attain righteousness will not receive it, but those who do not work
-      to attain righteousness will receive it. The work that does not obtain righteousness is our own work in the flesh
-      by the sweat of our brow. Our sweat was given to us as a punishment by God at the fall at the Garden; it can't
-      earn us favor with God; human flesh accomplishes nothing, it never pleased God and never will. But the work that
-      does please God is not a work at all: it's faith; faith is a work of the spirit by believing in Jesus Christ. So
-      then, where is our boasting? As it says,
+      It's an offense, a stumbling block, to say: those who work hard to attain righteousness do not receive it; but
+      those who do no work to attain righteousness do receive it. The work that does not obtain righteousness is our own
+      work; but the work that does obtain righteousness is the work of faith. Our work is a work of the flesh by the
+      sweat of our brow, but the work of faith is a work of the Spirit. Our toil was given to us as a punishment from
+      God at the fall at the Garden; our toil and labor produces sweat, but the work of faith does not. As it is
+      written, “Linen turbans shall be on their heads and linen undergarments shall be on their loins; they shall not
+      gird themselves with anything which makes them sweat.” (Ezekiel 44:18) And the priests of the Old Covenant wore
+      linen clothes when they ministered in the tabernacle; the Holy Spirit is signifying that the work of God is not
+      burdensome. Regarding the work of the law, it says, “Why do you put God to the test by placing upon the neck of
+      the disciples a yoke which neither our fathers nor we have been able to bear?” (Acts 15:10) But regarding the work
+      of faith, it says, “For this is the love of God, that we keep His commandments; and His commandments are not
+      burdensome.” (1 John 5:3) Human flesh accomplishes nothing, it never pleased God and never will. But the work that
+      pleases God is not a work at all: it's faith; faith is a work of the spirit by believing in Jesus Christ. So then,
+      where is our boasting? Is it in the flesh? No! But rather our boast is in God; as it says,
       <blog-reference-tooltip [reference]="'Jeremiah 9:24'">
-        But let him who boasts boast in this, that he understands and knows Me, that I am the LORD who practices
-        steadfast love, justice, and righteousness in the earth. For in these things I delight, declares the LORD. </blog-reference-tooltip
-      ><br /><br />
+        Let him who boasts boast in this, that he understands and knows Me, that I am the LORD who practices steadfast
+        love, justice, and righteousness in the earth. For in these things I delight, declares the LORD.
+      </blog-reference-tooltip>
+      “For we are the true circumcision, who worship in the Spirit of God and glory in Christ Jesus and put no
+      confidence in the flesh.” (Philippians 3:3)
+      <br /><br />
 
-      What then, if we have faith within, but nothing is worked out? Faith has works; works make faith complete. Faith
-      without works is dead, it is not faith at all, but sincere faith is living, for it is doing. That which is dead,
-      does nothing; that which is dead cannot save; can a mute idol save? Who but the living God can raise the dead? Who
-      can deliver but God? Sincere faith is like breathing; we do it naturally. We don't have to try so hard to breath,
-      we just do it. That's the same with sincere faith: we don't have to try so hard, but it comes naturaly. We believe
-      and so act. Be a child in regard to faith, and simply believe. What about blinking? You certainly don't strive
-      hard to blink! It happens naturally. If you want to lose weight, you can run a marathon or not eat; which is
-      easier? If you want to stay afloat, strive hard and kick or be at peace and float; which is less stress? Even if
-      you can exert enough strength to remain afloat for an hour, certainly not for a lifetime. But faith is for life
-      and strength only for a moment; as it says, “For His anger is but for a moment, His favor is for a lifetime;
-      Weeping may last for the night, But a shout of joy comes in the morning.” (Psalms 30:5) And the works of the flesh
-      brings forth wrath, but those who believe obtain favor from God. Those who exert strength for eternal life do not
-      obtain life, but those who rest in Christ find mercy from God. Strive to keep your faith sincere; this is a most
-      important thing. Do not try to make faith by simulating faith with works which do not come from faith; a simulator
-      is not actually the thing it is simulating, but only a fake, a robot. If you simulate being a Christian by doing
-      what your Christian friends do, that's not sincere; that's not really a Christian, that won't get you into
-      Paradise; the Lord sees your heart. Instead strive for faith in Christ; if you do not believe, pray that you may
-      believe; seek and He will let you find Him; the Father will lead you to the Son, then you will naturally do the
-      things which are fruits of faith. Know the nature of unbelief; saying, “The one who does not believe God has made
-      Him a liar, because he has not believed in the testimony that God has given concerning His Son.” (1 John 5:10)
-      But, "He who has received His testimony has set his seal to this, that God is true.” (John 3:33) And, "It is
-      impossible for God to lie.” (Hebrews 6:18) When I was young, I wanted to get out of school; I would simulate a
-      cold to trick my mom to let me stay home; certainly I was able to fool them, but what about God? I didn't actually
-      have a cold, I was only simulating one: I put the tissues on my nose, I put heat on my forehead, I disfigured my
-      face. I knew the truth and God knew the truth; but I first needed to deceive myself in order to deceive my mom; I
-      had to lie to myself before I lied to another. I can fool myself, but will simulating faith fool the Lord on
+      What then do we do, if we have faith within, but nothing is worked out? For our works make our faith complete.
+      Faith without works is dead; it is not faith at all, but sincere faith is living, for it is doing. That which is
+      dead, does nothing; and that which is dead cannot save; can a mute idol save even itself? Who but the living God
+      can raise the dead? Who can deliver but God? A sincere faith is like breathing - we do it naturally. We don't have
+      to try or toil so hard to breathe, we just do it. That's the same with sincere faith: we don't try so hard, but it
+      comes naturally. We believe and so act. Be a child in regard to faith - simply believe. What about blinking? You
+      certainly don't strive hard to blink! It happens naturally. If you want to lose weight, you can run a marathon or
+      eat nothing; which is easier? If you want to stay afloat, strive hard and kick or be at peace and float; which is
+      less stress? Even if you can exert enough strength to remain afloat for an hour, certainly you cannot for a
+      lifetime. Faith is for a lifetime, but human strength is only for a moment; as it says, “For His anger is but for
+      a moment, His favor is for a lifetime; Weeping may last for the night, But a shout of joy comes in the morning.”
+      (Psalms 30:5) For the work of the flesh brings forth wrath, but those who believe obtain favor with God. Those who
+      exert strength for eternal life do not obtain life, but those who rest in Christ find the mercy of God. Strive to
+      keep your faith sincere; this is a most important thing. Do not try to make faith by simulating faith with works;
+      but rather, have authentic faith which produces works. A simulator is not actually the thing it is simulating; it
+      is only a fake, a robot. If a robot mimics a human, does that make it a human? Of course not! If you simulate
+      being a Christian by doing what Christians do, that doesn't make you a Christian; that's not really faith, and
+      that won't get you into Paradise - the Lord sees your heart. When I was young, I wanted to get out of school; I
+      simulated a cold to trick my mom to let me stay home; certainly I was able to fool her. I didn't actually have a
+      cold, I was only simulating one: I put the tissues on my nose, I put heat on my forehead, I disfigured my face. I
+      knew the truth and God knew the truth; but I first needed to deceive myself in order to deceive my mom; I had to
+      lie to myself before I lied to another. I can fool myself, but will simulating faith fool the living God on
       judgment day? That is why it says,
       <blog-reference-tooltip [reference]="'Romans 14:22'">
         The faith which you have, have as your own conviction before God.
       </blog-reference-tooltip>
-      I must have faith of my own that the eternal Father was in Christ and died for me, that He has great love towards
-      me in Christ, faith, that He loved me to give me my present life and He loved me to give me new life. With faith,
-      I experience a change of heart, the hardness cut away. My life changed forever; the Lord changed my heart.<br /><br />
+      So strive for faith of your own in Christ; if you do not believe, pray that you may believe; seek and He will
+      graciously let you find Him; the Father will lead you to the Son, then you will naturally do the things which are
+      fruits of faith. I must have faith of my own, namely that, the eternal Father was in Christ and died for me, that
+      He has great love towards me in Christ; I must have my own faith in that He loved me to give me life and He loved
+      me to give me new life. With faith, I experienced a change of heart: the hardness of it cut away. My life was
+      changed forever; the Lord changed me from within. Know the nature of unbelief; as it says, “The one who does not
+      believe God has made Him a liar, because he has not believed in the testimony that God has given concerning His
+      Son.” (1 John 5:10) For, "It is impossible for God to lie.” (Hebrews 6:18) But, "He who has received His testimony
+      has set his seal to this, that God is true.” (John 3:33) <br /><br />
 
       From the beginning of time, God hid by mystery in His Word the way to be made right with Himself, which is now
-      revealed in Christ - a righteousness by faith. Jesus came down from heaven and healed men of their sickeness and
-      did good where-ever He went; the people who received healing were those who had faith in His name. They were not
-      healed because of their obedience to the law of Moses; no! They were healed because of their faith; as it says,
+      revealed in Christ: a righteousness by faith. Jesus came down from heaven and healed men of their sickness and did
+      good where-ever He went; the people who received healing were those who had faith in His name. They were not
+      healed because of their obedience to the law of Moses - no! But rather, they were healed because of their faith;
+      as it says,
       <blog-reference-tooltip [reference]="'Mark 5:34'" [isJesusWord]="true">
         Daughter, your faith has made you well; go in peace, and be healed of your disease.
       </blog-reference-tooltip>
@@ -72,29 +86,30 @@ import { SharedModule } from "../../../shared/shared.module";
       </blog-reference-tooltip>
       And there are many other such things He said (Luke 7:50, Mark 10:52, Luke 17:19, Matthew 8:13, Matthew 15:28).
       Abraham was reckoned righteous by believing in God's promise given by God's generosity. And it is no different for
-      us today: we are made right with God by believing the testimony about His Son, He is the fulfillment of the
+      us today: we are made right with God by believing the testimony about His Son. He is the fulfillment of the
       promise to Abraham; God has given us right standing with Himself and eternal life in His Son. After Abraham was
       counted righteous by God, He gave Abraham a sign of the covenant which is the circumcision of all males in the
-      flesh. And today, we have the same sign, but it's a spiritual circumcision, a spiritual procedure of putting away
-      the body of sins and removing the heart of stone; saying,
+      flesh. And today, we have the same sign, but it's a spiritual circumcision, made without hands, a spiritual
+      procedure of putting away the body of sins by removing the heart of stone; as it says,
       <blog-reference-tooltip [reference]="'Ezekiel 11:19'">
         And I will give them one heart, and a new spirit I will put within them. I will remove the heart of stone from
-        their flesh and give them a heart of flesh
+        their flesh and give them a heart of flesh.
       </blog-reference-tooltip>
       And,
       <blog-reference-tooltip [reference]="'Colossians 2:11'">
         In Him you were also circumcised with the circumcision made without hands, by putting off the body of the sins
-        of the flesh, by the circumcision of Christ
+        of the flesh, by the circumcision of Christ.
       </blog-reference-tooltip>
-      It is Christ who puts away the flesh of the body and takes away the hardness of heart; and the only good flesh is
-      the heart of flesh. Jesus is the Physician, and He does the procedure; it happens at the moment we believe the
+      It is Christ who puts away the flesh of the body and takes away the hardness of heart. The only good flesh is the
+      heart of flesh! Jesus is the Physician, and He does the procedure; it happens at the moment we believe the
       Testimony. And the second part of the procedure is the uniting of the Holy Spirit with our spirit; for God writes
-      on our new circumcised hearts His law by His Spirit; as it says,
+      on our new circumcised hearts His law, by His Spirit; as it says,
       <blog-reference-tooltip [reference]="'Jeremiah 31:33'">
         For this is the covenant that I will make with the house of Israel after those days, declares the Lord: I will
         put my law within them, and I will write it on their hearts. And I will be their God, and they shall be my
         people.
       </blog-reference-tooltip>
+      “For neither is circumcision anything, nor uncircumcision, but a new creation.” (Galatians 6:15)
     </ng-template>`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
