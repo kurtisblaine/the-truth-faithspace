ab8d9e588bb2710b17d5f9238726ef38a10703e6
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-05-28 17:46:00 -0500

    updates

--- apps/gospel/src/app/gospel-page/gospel-content/response/holiness.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-content/response/holiness.component.ts
@@ -7,103 +7,109 @@ import { SharedModule } from "../../../shared/shared.module";
   imports: [CommonModule, SharedModule],
   template: `<ng-container *ngTemplateOutlet="obedienceToHoliness"></ng-container>
     <ng-template #obedienceToHoliness>
-      We are made holy by the Holy Spirit of God, if indeed the Spirit of God dwells in you. Now if anyone does not have
-      the Spirit of Christ, he is not His (Romans 8:9-10); He is the Helper; He is the Spirit of holiness. Our bodies
-      are temples of the living God; and it's God's presence with us that sets us apart from the rest of the world. Or
-      do you think it is oil that consecrates? What does the Scriptures say?
+      Our bodies are temples of the living God; and it's God's presence with us that sets us apart from the rest of the
+      world. Or do you think it's the oil that consecrates? What do the Scriptures say? As Moses said to God,
       <blog-reference-tooltip [reference]="'Exodus 33:15-16'">
-        Then he (Moses) said to Him, “If Your presence does not go with us, do not lead us up from here. For how then
-        can it be known that I have found favor in Your sight, I and Your people? Is it not by Your going with us, so
-        that we, I and Your people,
-        <b>may be distinguished from all the other people who are upon the face of the earth?</b>”
+        If Your presence does not go with us, do not lead us up from here. For how then can it be known that I have
+        found favor in Your sight, I and Your people? Is it not by Your going with us, so that we, I and Your people,
+        <b>may be distinguished from all the other people who are upon the face of the earth?</b>
       </blog-reference-tooltip>
-      And,
+      And, the LORD God says,
       <blog-reference-tooltip [reference]="'Exodus 29:43'">
         I will also meet with the Israelites there, and that place will be consecrated by My
         glory.</blog-reference-tooltip
       >
-      Was it not the Gentiles who seen Abraham, Isaac, and Jacob, that God was with them? As it says,
+      So it is God's presence that sets us apart from everyone else. Just as Balaam, the son of Beor, seen and
+      testified, "As I see him from the top of the rocks, And I look at him from the hills; Behold, a people who dwells
+      apart, And will not be reckoned among the nations.” (Numbers 23:9) Furthermore, the Gentiles seen that God was
+      with Abraham, Isaac, and Jacob; and sought their favor. As the Scriptures say,
       <blog-reference-tooltip [reference]="'Genesis 21:22'">
         Now it came about at that time that Abimelech and Phicol, the commander of his army, spoke to Abraham, saying,
         "God is with you in all that you do;"
       </blog-reference-tooltip>
-      And,
+      And they said to Isaac,
       <blog-reference-tooltip [reference]="'Genesis 26:28'">
-        But they said, “We have certainly seen that the LORD is with you. So we said, ‘Let there now be an oath between
-        us, between you and us; and let us make a covenant with you"
+        We have certainly seen that the LORD is with you. So we said, ‘Let there now be an oath between us, between you
+        and us; and let us make a covenant with you.'
       </blog-reference-tooltip>
-      The Word and our faith, which is our obedience to Him, leads to holiness; we correct ourselves to the Word of God,
-      that is obedience, that is obedience to the truth; it is not obedience to Torah law, but obedience to truth, that
-      is Christ. We don't start by the Spirit and try to be made perfect by the flesh, but we are to be rooted in the
-      truth; obedience shows we have faith, and it's a living faith to obey the truth. A dead faith does not save
-      anybody; we are to prove ourselves, to show we are in the truth. <br /><br />
+      So it is: God is holy; and He alone sanctifies. He is God, and He calls us to godliness. Therefore,
+      <blog-reference-tooltip [reference]="'1 Peter 1:14'">
+        As obedient children, do not be conformed to the passions of your former ignorance, but as he who called you is
+        holy, you also be holy in all your conduct, since it is written, You shall be holy, for I am holy.
+      </blog-reference-tooltip>
+      <br /><br />
 
-      The oil of consecration is a symbol of the consecration which would come from God in Christ Jesus. First, blood
-      was applied to the people and the objects used in worship; blood cleansed the items and the people for worship
-      just as water cleanses physical things. Then oil is applied to those objects to set them apart for ministering to
-      the Lord. Likewise, we are first cleansed by the blood, and Christ sanctifies us by His teachings; He sets us
-      apart by the Holy Spirit for service; and it's God's Spirit who sanctifies, for He alone speaks the Word of God;
-      anyone who has not the Spirit of the Lord cannot speak the Word of the Lord or even understand it, for it is
-      spiritually discerned. The anointing of God makes us to live for Him in willing service; for all of God's saints
-      willingly serve as bond-servants to God by the compulsion of the Spirit. For we are no longer bound by the
-      obligation of the law, but freely offer ourselves to God as alive from the dead. By faith God imparts to us the
-      Holy Spirit; for the Word is united in us by faith (Hebrews 4:2); and if the Word of God be in us, then the Spirit
-      of God. It is God's Spirit that sets us apart and makes us fit for service in His kingdom; as it says,
+      So the oil of consecration is only a symbol of the consecration which comes from God. As the apostle says, “But
+      you have an anointing from the Holy One, and you all know.” (1 John 2:20) In the law, blood was first applied to
+      the people and the objects used in worship; blood cleansed the items and the people for worship just as water
+      cleanses physical things. Then oil is applied to those objects to set them apart for ministering to the Lord. And
+      likewise, we are first cleansed by the blood, and Christ sanctifies us by His presence; He sets us apart by the
+      Holy Spirit for service; and it's God's Spirit who sanctifies. As it says,
       <blog-reference-tooltip [reference]="'Romans 15:16'">
-        to be a minister of Christ Jesus to the Gentiles in the priestly service of the gospel of God, so that the
-        offering of the Gentiles may be acceptable, <b>sanctified by the Holy Spirit.</b>
+        So that the offering of the Gentiles may be acceptable, <b>sanctified by the Holy Spirit.</b>
       </blog-reference-tooltip>
-      And the truth sanctifies us, the obedience of faith; as He says,
+      For “He saved us, not on the basis of deeds which we have done in righteousness, but according to His mercy,
+      <b>by the washing of regeneration and renewing by the Holy Spirit.</b>” (Titus 3:5) Anyone who does not have the
+      Spirit of the Lord, he cannot speak the Word of the Lord, or even understand it, for it is spiritually discerned.
+      The Spirit of Truth speaks spiritual words of truth - and by the truth we are sanctified. As Jesus says,
       <blog-reference-tooltip [reference]="'John 17:17-19'" [isJesusWord]="true">
         <b>Sanctify them by the truth; your word is truth.</b> As you sent me into the world, I have sent them into the
         world. For them I sanctify myself, that they too may be sanctified in truth.
       </blog-reference-tooltip>
-      And,
+      And, as the apostle says,
+      <blog-reference-tooltip [reference]="'Ephesians 5:25-27'">
+        Husbands, love your wives, just as Christ also loved the church and gave Himself up for her,
+        <b>so that He might sanctify her, having cleansed her by the washing of water with the word. </b>
+      </blog-reference-tooltip>
+      However, if we do not believe the Word of God, then the truth is not united in us; for without faith,
+      sanctification is impossible. As Jesus says,
       <blog-reference-tooltip [reference]="'Acts 26:18'" [isJesusWord]="true">
-        to open their eyes so that they may turn from darkness to light and from the dominion of Satan to God, that they
-        may receive forgiveness of sins and an inheritance among those
+        that they may receive forgiveness of sins and an inheritance among those
         <b>who have been sanctified by faith in Me.</b>
       </blog-reference-tooltip>
-      And,
+      And, as the apostle says,
       <blog-reference-tooltip [reference]="'2 Thessalonians 2:13'">
-        But we should always give thanks to God for you, brethren beloved by the Lord, because God has chosen you from
-        the beginning for salvation through sanctification by the Spirit and faith in the truth.
+        God has chosen you from the beginning for salvation
+        <b>through sanctification by the Spirit and faith in the truth.</b>
       </blog-reference-tooltip>
-      Or even,
-      <blog-reference-tooltip [reference]="'Ephesians 5:25-27'">
-        Husbands, love your wives, just as Christ also loved the church and gave Himself up for her, so that He might
-        sanctify her, having cleansed her by the washing of water with the word, that He might present to Himself the
-        church in all her glory, having no spot or wrinkle or any such thing; but that she would be holy and blameless.
-      </blog-reference-tooltip>
-      And the truth is the gospel, that of Christ's blood; saying,
+      But what have we come to believe in? What is the knowledge? What is the message of truth? As it says,
       <blog-reference-tooltip [reference]="'Hebrews 13:12'">
-        Therefore Jesus also, that He might sanctify the people through His own blood, suffered outside the gate.
+        Therefore Jesus also, that He might <b>sanctify</b> the people through His own blood, suffered outside the gate.
       </blog-reference-tooltip>
-      “By His doing you are in Christ Jesus, who became to us wisdom from God, and righteousness and sanctification, and
-      redemption, so that, just as it is written, "LET HIM WHO BOASTS, BOAST IN THE LORD.” (1 Corinthians 1:30-31) And,
-      “He saved us, not on the basis of deeds which we have done in righteousness, but according to His mercy, by the
-      washing of regeneration and renewing by the Holy Spirit.” (Titus 3:5)
-      <br /><br />
+      And, “By His doing you are in Christ Jesus, who became to us wisdom from God,
+      <b>and righteousness and sanctification,</b> and redemption, so that, just as it is written, "LET HIM WHO BOASTS,
+      BOAST IN THE LORD.” (1 Corinthians 1:30-31) For it is the Lord's work to sanctify us; it is our work to believe
+      the message of God.<br /><br />
 
-      If we obey God, we will be holy like Him, for He alone is holy; His Word is holy. Those who wish to be used
-      honorably of Him must themselves be holy. Unholy talk is that which is common in the world: meaningless chatter,
-      myths, fables, anything that doesn't tell of the blessed God and Savior. No-one can tell of Christ except by the
-      Holy Spirit, which is the spirit of prophesy. The spirit of this world which is also the spirit of the antichrist
-      and the spirit of lawlessness, is common, not from above, but below. But the Spirit which is from above is holy,
-      which you have received if you have received the Word of Truth. If we purify ourselves from the common (the world
-      and its filthy things) we will be set apart for holy use; as it says,
+      If we obey God, it will lead to holiness; and we will be holy like Him, for He alone is holy. Those who wish to be
+      used honorably of Him must themselves be holy. Unholy talk is that which is common in the world: meaningless
+      chatter, myths, fables, anything that doesn't tell of the blessed God and Savior. No-one can tell of Christ except
+      by the Holy Spirit, which is the spirit of prophecy. The Spirit which is from above is holy, which you have
+      received if you have received the Word of Truth. If we purify ourselves from the world and its filth, we will be
+      set apart for holy use; as it says,
       <blog-reference-tooltip [reference]="'2 Timothy 2:21'">
         Therefore if anyone cleanses himself from the latter (common articles), he will be a vessel for honor,
         sanctified and useful for the Master, prepared for every good work.
       </blog-reference-tooltip>
-      Obedience leads to holiness, righteousness to sanctification; and with holiness comes salvation. Consecration is
-      moving from what is earthly to what is heavenly, from what is bad to what is good. We do not belong to ourselves,
-      but to God, who bought us with a price. We live not to please ourselves, but Jesus, who loved us; and He lives in
-      us by the Spirit of Truth - the Holy Spirit is the Spirit of Truth. We do not belong to ourselves: we have been
-      purchased. Our life did not belonged to us in the first place, because everything belongs to the LORD, but we
-      willingly submit ourselves to Him as bond-servants: willing to serve and do His will. I write concerning holiness
-      of the heart and the tongue, but it works outwardly to the holiness of the body as well; as the brother, Paul,
-      admonishes,
+      Obedience leads to holiness, righteousness leads to sanctification; and with holiness comes salvation. As it says,
+      “Pursue peace with all men, and the sanctification without which no one will see the Lord.” (Hebrews 12:14)
+      Consecration is moving from what is earthly to what is heavenly, from what is bad to what is good. We do not
+      belong to ourselves, but we belong to God. We were bought with a price. We live not to please ourselves, but God
+      who loves us. Our life does not belong to us, for everything belongs to the LORD; but how much more so now that He
+      bought us? For when we were slaves of sin, we were constantly serving sin. But now, God calls us to be obedient
+      from the heart. We ought to willingly submit ourselves to Him as bond-servants, putting our ear to the awl (Exodus
+      21:5-6), saying, "I love my master, my wife and my children; I will not go out as a free man.” (Exodus 21:5) Or
+      saying, as the Psalmist says, “My life is continually in my hand, Yet I do not forget Your law.” (Psalms 119:109)
+      For we were released from our captivity to the law, and now use our freedom to serve God in righteousness. For the
+      law of God either binds the disobedient or grants freedom to the obedient. As James calls the law, "The perfect
+      law, the law of liberty." (James 1:25) But the apostle Paul calls the law, "The ministry of death" (2 Corinthians
+      3:7) and, "The ministry of condemnation." (2 Corinthians 3:9) So if we fulfill the law, we live in the freedom,
+      and rest, and life of God; but if we disobey, we lie in the depths of slavery, and labor, and death in sin.
+      However, obedience does not come from the flesh, but from the Spirit through faith in Jesus - just as I have
+      written above. I write beforehand concerning holiness of the heart, but it works outwardly to the body as well.
+      But I ask, "What is more true: the body or the spirit?" Surely the spiritual is more true - but if anything be
+      true in spirit, it will be manifested physically too. As the apostle, Paul, commands in the name of the Lord
+      Jesus,
       <blog-reference-tooltip [reference]="'1 Thessalonians 4:3-8'">
         It is Godʼs will that you should be sanctified: that you should avoid sexual immorality; that each of you should
         learn to control your own body in a way that is holy and honorable, not in passionate lust like the pagans, who
@@ -112,24 +118,26 @@ import { SharedModule } from "../../../shared/shared.module";
         impure, but to live a holy life. Therefore, anyone who rejects this instruction does not reject a human being
         but God, the very God who gives you his Holy Spirit.
       </blog-reference-tooltip>
-      And,
       <blog-reference-tooltip [reference]="'1 Corinthians 6:18-20'">
         Flee from sexual immorality. Every other sin a man can commit is outside his body, but he who sins sexually sins
         against his own body. Do you not know that your body is a temple of the Holy Spirit who is in you, whom you have
         received from God? You are not your own; you were bought at a price. Therefore glorify God with your body.
       </blog-reference-tooltip>
-      And,
-      <blog-reference-tooltip [reference]="'1 Peter 1:14'">
-        As obedient children, do not be conformed to the passions of your former ignorance, but as he who called you is
-        holy, you also be holy in all your conduct, since it is written, You shall be holy, for I am holy.
+      Everyone who believes the Word, corrects themselves to it. Obey the truth. “Therefore, putting aside all
+      filthiness and all that remains of wickedness, in humility receive the word implanted, which is able to save your
+      souls.” (James 1:21) Obedience shows that we love God. As the Lord Jesus says,
+      <blog-reference-tooltip [reference]="'John 14:15'" [isJesusWord]="true">
+        If you love Me, you will keep My commandments.
       </blog-reference-tooltip>
-      Or even,
-      <blog-reference-tooltip [reference]="'1 Corinthians 7:34'">
+      And,
+      <blog-reference-tooltip [reference]="'John 15:10'" [isJesusWord]="true">
+        If you keep My commandments, you will abide in My love; just as I have kept My Father's commandments and abide
+        in His love.
+      </blog-reference-tooltip>
+      <!-- <blog-reference-tooltip [reference]="'1 Corinthians 7:34'">
         The woman who is unmarried, and the virgin, is concerned about the things of the Lord, that she may be holy both
         in body and spirit
-      </blog-reference-tooltip>
-      I say these things because it is possible to quench and grieve the Holy Spirit (1 Thessalonians 5:19, Ephesians
-      4:30).
+      </blog-reference-tooltip> -->
       <blog-link [title]="'See More on Sanctification'" [link]="'https://thelightof.life/#/draw/sanctification.webp'">
       </blog-link>
     </ng-template>`,
