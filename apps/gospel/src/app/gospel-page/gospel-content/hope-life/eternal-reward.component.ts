fb288ad247ae2bc9c1c528029c93348826182a24
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-05-30 14:13:41 -0500

    eternal reward.

--- apps/gospel/src/app/gospel-page/gospel-content/hope-life/eternal-reward.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-content/hope-life/eternal-reward.component.ts
@@ -7,15 +7,15 @@ import { SharedModule } from "../../../shared/shared.module";
   imports: [CommonModule, SharedModule],
   template: `<ng-container *ngTemplateOutlet="eternalReward"></ng-container>
     <ng-template #eternalReward>
-      All of us in Christ Jesus, have been forgiven of our sins, the debt that was in our account which testified
-      against us, stored up under the former covenant when we walked according to the flesh, we were by nature children
-      of wrath, born in trouble and for trouble; and now, having been cleared, God credits into our accounts gifts of
-      the Spirit so that we would profit, being profitable through wisdom unto salvation, through the knowledge of the
-      will of the Lord. The stewardship we have is to build up and to edify the church, and also bartering, trading and
+      In Christ Jesus have been forgiven of our sins, the debt that was in our account which testified against us,
+      stored up under the former covenant when we walked according to the flesh, for we were by nature children of
+      wrath, born in trouble and for trouble; but now, having been cleared, God credits into our accounts gifts of the
+      Spirit so that we would profit, being profitable through wisdom unto salvation, through the knowledge of the will
+      of the Lord. The stewardship we have is to build up and to edify the church, and also bartering, trading and
       winning more, sharpening and being sharpened. Paul, being a master builder, never built on another man's
-      foundation, but laid his own, so he would know what kind of foundation it is. Let us beware that we not build on
-      any foundation other than the one that has been already laid: Jesus Christ. He is the Rock of our salvation, the
-      firm foundation, the Chief cornerstone; and he warns,
+      foundation, but laid his own, so that he may know for sure what kind the foundation is. Let us beware that we not
+      build on any foundation other than the one that has been already laid: Jesus Christ, the Rock of our salvation,
+      the firm foundation, the Chief cornerstone. As it says,
       <blog-reference-tooltip [reference]="'1 Corinthians 3:12-15'">
         If anyone builds on this foundation using gold, silver, costly stones, wood, hay or straw, their work will be
         shown for what it is, because the Day will bring it to light. It will be revealed with fire, and the fire will
@@ -24,73 +24,74 @@ import { SharedModule } from "../../../shared/shared.module";
         the flames.
       </blog-reference-tooltip>
       Know for certain: if we partake in the resurrection from the dead, the true one, the first one, then the second
-      death holds no power. That means, we will not be raised to punishment, but to reward, for punishment holds no
-      power to those who are perfected in love, who are children of the first resurrection; likewise it says,
+      death holds no power over us. That means, we will not be raised to punishment, but to reward, for punishment holds
+      no power to those who are perfected in love; as it says,
       <blog-reference-tooltip [reference]="'1 John 4:17-18'">
         By this, love is perfected with us, so that we may have confidence in the day of judgment; because as He is, so
         also are we in this world. There is no fear in love; but perfect love casts out fear, because fear involves
         torment. But he who fears has not been made perfect in love.
       </blog-reference-tooltip>
-      We can have confidence in that day and great assurance in the faith if we do. As it says, "For those who have
-      served well as deacons obtain for themselves a good standing and great boldness in the faith which is in Christ
-      Jesus." (1 Timothy 3:13) And, "Now, little children, abide in Him, so that when He appears, we may have confidence
-      and not shrink away from Him in shame at His coming." (1 John 2:28) Faith is not shaky, but confident; when we
-      prove ourselves, we gain confidence. So prove yourselves that you belong to Christ Jesus, always serving in love,
-      especially to the household of believers.
+      So therefore, we can have confidence in that day and great assurance in the faith if we do; as it says, "For those
+      who have served well as deacons obtain for themselves a good standing and great boldness in the faith which is in
+      Christ Jesus." (1 Timothy 3:13) And, "Now, little children, abide in Him, so that when He appears, we may have
+      confidence and not shrink away from Him in shame at His coming." (1 John 2:28) For faith is not shaky, but
+      confident; when we prove ourselves, we gain confidence. So prove yourselves. Prove yourself that you belong to
+      Christ Jesus, always showing love to all people, especially to the household of believers.
       <br /><br />
 
-      If we are faithful with little, we will be faithful with much; and to the faithful, Christ Jesus will put in
-      charge of the nations (Matthew 25:14-30). This is good stewardship: taking good care of what He has entrusted to
-      us. To be profitable, you may have to gamble. Those who play the slot machine spend some in order to win some.
-      No-one plays to lose, but to win. Do all slot machines hit the jackpot? Is all soil good and fertile? Of course
-      not. If the soil is bad, the seed casted on the soil is wasted; there is nothing wrong with the seed, but the soil
-      is the problem; likewise, if we don't profit, the seed is wasted; if it produces a yield, we hit the jackpot, for
-      a single seed can produce even a hundred times as much! Does one know beforehand whether the soil is going to be
-      good or bad? Surely if it produces thorns and thistles, we have a good idea it's bad. What about if a machine will
-      win? If so, wouldn't we always win? If we knew, there would be no room for chance and everything would yield - we
-      would always profit. God is sovereign in all areas - much of His will is revealed by chance. He provides the
-      growth of the seed, and He makes the lot fall. We trust Him as we operate this dark world - for He is our Light.
-      If we refuse to build up our brother, instead wasting our seed on those who don't hear us, how are we any
-      different than Onan? Who fell into the condemnation as follows,
+      If we are faithful with a little, we will be faithful with much; and to the faithful, Christ Jesus will put in
+      charge of the nations (Matthew 25:14-30). This is good stewardship. So take good care of what He has entrusted to
+      you. In order to be profitable, you must gamble. Those who play the slot machine spend some in order to win more.
+      No-one plays to lose, but they play to win. I say, "Gamble" because not all machines are winners. You don't hit
+      the jackpot everytime, do you? Is all soil good and fertile? Of course not! And if the soil is bad, the seed
+      casted on the soil is wasted, although there is nothing wrong with the seed. Likewise, if we don't profit, the
+      money is wasted; but if it produces a yield, we have hit the jackpot! A single coin can produce hundreds of dollar
+      bills; a single seed can produce a hundred times at harvest. Do we know beforehand whether the soil is going to be
+      good or bad? Surely if it produces thorns and thistles, we have a good idea.
+      <blog-reference-tooltip [reference]="'Matthew 7:6'" [isJesusWord]="true">
+        Do not give what is holy to dogs, and do not throw your pearls before swine, or they will trample them under
+        their feet, and turn and tear you to pieces.
+      </blog-reference-tooltip>
+      God is sovereign in all areas - and much of His will is revealed in chance. He makes the lot fall, and He makes
+      the soil fertile, He gives the seed its growth. We trust in Him as we operate in this dark world - for He is our
+      Light. Take heed: if we refuse to build up our brother, and instead waste our seed, how are we any better than
+      Onan, who fell into condemnation; as it says,
       <blog-reference-tooltip [reference]="'Genesis 38:9-10'">
         Onan knew that the offspring would not be his; so when he went in to his brother's wife, he wasted his seed on
         the ground in order not to give offspring to his brother. But what he did was displeasing in the sight of the
         LORD; so He took his life also.
       </blog-reference-tooltip>
-      For he deliberately wasted his seed. While I am saying these things, I am assuming the seed is good and that the
-      money is good. Can anything good come from a counterfeit coin? Or is there any profit in a corrupt seed? Can
-      anything be better than what God has already provided? There is a bad seed which is called a lie, a false
-      confession. Does someone attempting to sell a car speak dishonestly in his ad? No! Otherwise the buyer will come
-      and see the car, and he will hate you, seeing that you have lied to him; he will be angry with you because you
-      wasted his time. He will not believe anything you say afterwards, even though you give many promises; He will
-      scrutinize your company; He will think badly of all car salesmen because of you. He will say to himself, "Those
-      car salesmen are no good! That company is dishonest, I can't trust them." We should never have someone gamble in
-      believing through us, but we ought to pour forth the truth in sincerity, casting forth our seed that they may
-      believe through us in Him; and we sell Christ - not that we sell Him who was freely given - but, convincing
-      others, telling of Christ in truth that He gave Himself for us, so that we would no longer live for ourselves, but
-      for Christ Jesus who purchased us; He was freely given, sold for us to redeem us from death, buying us so that we
-      would be brought to Another in the service of His ministry. If we sell Christ and we do not tell of the cost, we
-      lie. If we tell of the cost and not of the grace of Christ, we lie. Christ gave Himself, so therefore we give
-      ourselves, this is obedience to the truth. Where is the grace? Where is the power to do? Is it not in Christ? How
-      can anyone do without Christ first working in us? And is not our purpose in life to do His will? Therefore, we
-      obey the truth. How can anyone obey what they do not believe? And this is the truth: Christ gave Himself for us,
-      that we would be His possession, that we would no longer live for ourselves, that we would gain the inheritance.
-      So we see how evil a lie is! Much more when mingled with the truth, for it ruins the testimony; that is why it
-      says,
-      <blog-reference-tooltip [reference]="'Revelation 22:15'">
-        Outside are the dogs and the sorcerers and the immoral persons and the murderers and the idolaters,
-        <b>and everyone who loves and practices lying.</b>
-      </blog-reference-tooltip>
-      The Master loves His servants and cares for them; His servants are part of His body and the mind of His Spirit
-      commands them to do. A lie destroys the reputation of Him that we represent, and we represent Christ. Make sure
-      your seed is good, that you are being taught by the Son of God, that you practice first before casting seed, lest
-      you be disqualified for the prize in the end, having disqualified yourself by not obeying the truth firstly. Know
-      for certain, what the heart is full of, the mouth speaks; and whoever loves and practices a lie will not inherit
-      the Kingdom of God. If our heart does not condemn us, we have great confidence; if we have come to believe, we
-      have the confession of Christ within us.<br /><br />
+      For he deliberately wasted his seed and did not build up his brother's house. "If anyone does not provide for his
+      own, and especially for those of his household, he has denied the faith and is worse than an unbeliever.” (1
+      Timothy 5:8)
+      <br /><br />
 
-      Our reward is based on our overcoming; we will be rewarded based on what we do; as the Lord Jesus says,
-      <blog-reference-tooltip [reference]="'Revelation 2:1-3:, 21'" [isJesusWord]="true">
+      So therefore, “Sow with a view to righteousness, Reap in accordance with kindness; Break up your fallow ground,
+      For it is time to seek the LORD Until He comes to rain righteousness on you.” (Hosea 10:12) Prepare the good seed
+      which the Lord God will give you. For your own seed is not good enough. Can anything be better than what God has
+      provided? There is a bad seed which is called a lie, a false confession, a polluted spring. I ask: does a salesmen
+      attempting to sell a car speak dishonestly in an ad? Maybe it's so; but the buyer will eventually come to see the
+      car, and he will hate you, seeing that you lied to him; he will be angry with you because you wasted his time. He
+      will not believe anything you say afterwards, even though you make many promises; He will scrutinize your company;
+      He will think badly of all car salesmen because of you. He will say to himself, "Those car salesmen are no good!
+      That company is dishonest, I can't trust them." Therefore, we should never make someone gamble when believing
+      through us, but we ought to pour forth truth in sincerity, casting forth our seed with tears that we may reap in
+      joy. And we sell Christ - not that we sell Him who was freely given - but, convincing others, telling of Christ in
+      truth; that He gave Himself for us, so that we would no longer live for ourselves, but for Him who purchased us;
+      for He was freely given for us to buy us back who have sold ourselves to sin, so that we may dwell with Him. If we
+      sell Christ in this way, we will be fruitful. But if we do not tell of the cost of following Christ, we lie. If we
+      tell of the cost and not about the grace of Christ, we likewise lie. Christ gave Himself so that we would give
+      ourselves to Him; and this is obedience to the truth. For I ask: where is the grace? And where is the power to do?
+      Is it not in Christ? How can anyone do anything profitable without Christ in us? Is not our purpose in life to
+      seek and do His will? Therefore, we obey the truth. I ask: How can anyone obey what they do not believe? Christ
+      gave Himself for us, that we would be His possession, and that we would no longer live for ourselves, but live for
+      Him who bought us. He has loved us and came down from heaven to redeem us. Our Master loves and cares for the
+      servants, for the servants are a part of His own body; and He has made the mind of His Spirit to dwell with them
+      to remind them to do.
+      <br /><br />
+
+      Our reward is based on our overcoming; we will be rewarded based on what we do; as the Lord says,
+      <blog-reference-tooltip [reference]="'Revelation 2:1-3:22'" [isJesusWord]="true">
         <b>To him who overcomes</b> I will give to eat from the tree of life, which is in the midst of the Paradise of
         God. (...)<b>He who overcomes</b> shall not be hurt by the second death. (...) <b>To him who overcomes</b> I
         will give some of the hidden manna to eat. And I will give him a white stone, and on the stone a new name
@@ -106,20 +107,25 @@ import { SharedModule } from "../../../shared/shared.module";
         <b>He who overcomes will inherit these things</b> (the new heavens and the new earth), and I will be his God and
         he will be My son.
       </blog-reference-tooltip>
-      Similarily, the one who did not do anything with his talent was cast into outer darkness (Matthew 25:24-30); and
-      the one who did not feed the hungry or clothe the poor or visit the prisoner did not receive the inheritance
-      (Matthew 25:41-46). However, the ones who did these things entered into life. Likewise David the Psalmist said,
-      “The LORD has rewarded me according to my righteousness; According to the cleanness of my hands He has recompensed
-      me. For I have kept the ways of the LORD, And have not wickedly departed from my God. For all His ordinances were
-      before me, And I did not put away His statutes from me. I was also blameless with Him, And I kept myself from my
-      iniquity. Therefore the LORD has recompensed me according to my righteousness, According to the cleanness of my
-      hands in His eyes.” (Psalms 18:20-24) And the promise which the Lord gives to those who lose much to pick up the
-      burden of the gospel; He says,
+      Beware! The one who did not do anything with his talent was cast into outer darkness (Matthew 25:24-30). And the
+      one who did not feed the hungry or clothe the poor or visit the prisoner did not receive the inheritance (Matthew
+      25:41-46); but the ones who did these things entered into life. Likewise David the Psalmist said, “The LORD has
+      rewarded me according to my righteousness; According to the cleanness of my hands He has recompensed me. For I
+      have kept the ways of the LORD, And have not wickedly departed from my God. For all His ordinances were before me,
+      And I did not put away His statutes from me. I was also blameless with Him, And I kept myself from my iniquity.
+      Therefore the LORD has recompensed me according to my righteousness, According to the cleanness of my hands in His
+      eyes.” (Psalms 18:20-24) And in another place, he says, “The LORD judges the peoples; Vindicate me, O LORD,
+      according to my righteousness and my integrity that is in me.” (Psalms 7:8) So the reward is granted unto us, not
+      merely by what we believe, but by what we do. For just as at harvest time, the harvesters seek not for leaves, but
+      fruit; for at the harvest, only fruit matters. And so, if we want to be rewarded by the Lord, today and forever,
+      we must be obedient to His call; as the Lord promises,
       <blog-reference-tooltip [reference]="'Mark 10:29-30'" [isJesusWord]="true">
         Truly I say to you, there is no one who has left house or brothers or sisters or mother or father or children or
-        farms, for My sake and for the gospel's sake, but that he will receive a hundred times as much now in the
-        present age, houses and brothers and sisters and mothers and children and farms, along with persecutions; and in
-        the age to come, eternal life.
+        farms, for My sake and for the gospel's sake,
+        <b
+          >but that he will receive a hundred times as much now in the present age, houses and brothers and sisters and
+          mothers and children and farms, along with persecutions; and in the age to come, eternal life.</b
+        >
       </blog-reference-tooltip>
     </ng-template>`,
   styles: ``,
