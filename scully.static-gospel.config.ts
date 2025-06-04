a0787fcd06423f139a605dcfa142ede7b031f6be
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-06-04 13:05:50 -0500

    update scully...

--- scully.static-gospel.config.ts
+++ scully.static-gospel.config.ts
@@ -1,5 +1,7 @@
 import { ScullyConfig } from "@scullyio/scully";
 
+import "@scullyio/scully-plugin-puppeteer";
+
 export const config: ScullyConfig = {
   projectRoot: "./apps/gospel/public",
   projectName: "static-gospel",
@@ -26,5 +28,21 @@ export const config: ScullyConfig = {
       type: "json",
       url: "https://firestore.googleapis.com/v1/projects/blog-46974/databases/(default)/documents/proverb",
     },
+  },
+  puppeteerLaunchOptions: {
+    args: [
+      "--disable-gpu",
+      "--renderer",
+      "--no-sandbox",
+      "--no-service-autorun",
+      "--no-experiments",
+      "--no-default-browser-check",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-extensions",
+    ],
   },
 };
