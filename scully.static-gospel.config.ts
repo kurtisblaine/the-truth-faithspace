c305112973f00a8733fc3ed6b80b28072059efbf
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-06-06 13:04:07 -0500

    update static....

--- scully.static-gospel.config.ts
+++ scully.static-gospel.config.ts
@@ -1,7 +1,7 @@
 import { ScullyConfig } from "@scullyio/scully";
 
+import "@scullyio/scully-plugin-playwright";
 import "@scullyio/scully-plugin-puppeteer";
-//TODO: missing plugin?
 
 export const config: ScullyConfig = {
   projectRoot: "./apps/gospel/public",
