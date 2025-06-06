6912372c91aea4c9daa85f1f827a1083fdf3108b
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-06-05 21:01:30 -0500

    ugg change this again.

--- apps/gospel/src/app/gospel-page/gospel-page.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-page.component.ts
@@ -64,7 +64,7 @@ export class GospelPageComponent implements OnInit, AfterViewInit, OnDestroy {
       loopHorizontal: false,
       loopBottom: false,
       loopTop: false,
-      credits: { enabled: true, label: "God bless you.", position: "left" },
+      credits: { enabled: true, label: "May the grace of the Lord be with you.", position: "left" },
     };
   }
 
