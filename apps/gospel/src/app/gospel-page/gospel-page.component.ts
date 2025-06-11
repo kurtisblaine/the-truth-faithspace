65fd61a61dd1255a988c2730b6848faffd67855b
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-06-11 16:48:06 -0500

    Updates to ssg

--- apps/gospel/src/app/gospel-page/gospel-page.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-page.component.ts
@@ -1,13 +1,105 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { Location } from "@angular/common";
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnDestroy, OnInit, signal } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { isScullyRunning } from "@scullyio/ng-lib";
+import { fullpageApi, fullpageOptions, Item, Trigger } from "fullpage.js/dist/fullpage.extensions.min";
+import { DeviceDetectorService } from "ngx-device-detector";
+import { distinctUntilChanged, filter, Subscription } from "rxjs";
+import { SeoBaseComponent } from "../shared/components/seo-base/seo-base.component";
+
+type TItem = { isActive: boolean } & Item;
 
 @Component({
   selector: "gospel-gospel-page",
   standalone: false,
   templateUrl: "./gospel-page.component.html",
   styleUrl: "./gospel-page.component.scss",
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default, //requires 'Default' so the audio player buttons will reset.
 })
-export class GospelPageComponent {
-  public renderUserExperience = !isScullyRunning();
+export class GospelPageComponent extends SeoBaseComponent implements OnInit, AfterViewInit, OnDestroy {
+  public config: fullpageOptions = {
+    licenseKey: "GM477-9I82I-1L8K9-194JK-TJUVR",
+
+    controlArrows: false,
+    navigation: true,
+    navigationPosition: "right",
+    slidesNavigation: true,
+    slidesNavPosition: "bottom",
+    recordHistory: true,
+
+    afterLoad: this.afterPageLoad.bind(this),
+    afterSlideLoad: this.afterSlideLoad.bind(this),
+    lockAnchors: true,
+    scrollOverflow: false,
+    normalScrollElements: ".card-content, .mat-drawer",
+
+    scrollBar: true,
+    loopHorizontal: false,
+    loopBottom: false,
+    loopTop: false,
+    credits: { enabled: true, label: "May the Lord keep you.", position: "left" },
+  };
+
+  public isMobile = signal(false);
+  public isScullyRunning = isScullyRunning();
+
+  public fullpageApi: fullpageApi;
+  protected override keywords: string = "gospel, faith, righteous, live, kingdom, Jesus, revealed, truth";
+
+  private routeSubscription: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private deviceDetector: DeviceDetectorService
+  ) {
+    super();
+  }
+
+  ngOnInit() {
+    this.isMobile.set(this.deviceDetector.isMobile());
+  }
+
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
+  }
+
+  getApi(fullPageRef: fullpageApi) {
+    this.fullpageApi = fullPageRef;
+
+    this.routeSubscription = this.route.fragment
+      .pipe(
+        filter((fragment) => !!fragment),
+        distinctUntilChanged()
+      )
+      .subscribe((fragment) => {
+        const [sectionId, slideId] = fragment.split("/");
+        this.toAnchor(sectionId, slideId);
+      });
+  }
+
+  public toAnchor(sectionId: string, slideId: string = "") {
+    this.fullpageApi.moveTo(sectionId, slideId);
+  }
+
+  private afterPageLoad(origin: TItem, destination: TItem, direction: string, trigger: Trigger) {
+    if (!direction || !destination.isActive) return;
+
+    let pageAnchor = destination.anchor?.toString();
+    this.navigateToFragment(pageAnchor, "");
+  }
+
+  private afterSlideLoad(section: TItem, origin: TItem, destination: TItem, direction: string, trigger: Trigger) {
+    if (!direction || !destination.isActive) return;
+
+    const sectionId = section.anchor?.toString();
+    const slideId = destination.anchor?.toString();
+    this.navigateToFragment(sectionId, slideId);
+  }
+
+  private navigateToFragment(sectionId: string, slideId: string = "") {
+    const fragment = slideId ? `${sectionId}/${slideId}` : sectionId;
+
+    this.location.go(`/truth#${fragment}`);
+  }
 }
---
+++ apps/gospel/src/app/shared/directives/fullpage/fullpage.directive.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Directive, EventEmitter, Input, OnDestroy, Output, Renderer2 } from "@angular/core";
+import { isScullyRunning } from "@scullyio/ng-lib";
+
+declare let fullpage: any;
+
+@Directive({
+  selector: "[gospelFullpage]",
+})
+export class FullpageDirective implements AfterViewInit, OnDestroy {
+  @Input() id: string;
+  @Input() options;
+  @Output() ref = new EventEmitter();
+
+  public fullpageApi;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    if (isScullyRunning()) return;
+
+    this.initFullpage();
+  }
+
+  initFullpage() {
+    this.fullpageApi = new fullpage("#" + this.id, this.options);
+    // this.addBuildFunction();
+    this.ref.emit(this.fullpageApi);
+  }
+
+  addBuildFunction() {
+    this.fullpageApi.build = () => {
+      const activeSection = this.fullpageApi.getActiveSection();
+      const activeSlide = this.fullpageApi.getActiveSlide();
+      this.destroyFullpage();
+      if (activeSection) {
+        this.renderer.addClass(activeSection.item, "active");
+      }
+      if (activeSlide) {
+        this.renderer.addClass(activeSlide.item, "active");
+      }
+      this.initFullpage();
+    };
+  }
+
+  destroyFullpage() {
+    if (typeof this.fullpageApi !== "undefined" && typeof this.fullpageApi.destroy !== "undefined") {
+      this.fullpageApi.destroy("all");
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroyFullpage();
+  }
+}
