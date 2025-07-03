59e32849df333c7b1fd4f24bd547be6c11ba46ad
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-07-03 14:32:31 -0500

    remove zone.js and use zoneless..

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -1,12 +1,11 @@
 import { provideHttpClient } from "@angular/common/http";
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { ApplicationConfig, provideZonelessChangeDetection } from "@angular/core";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router";
 import { appRoutes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       appRoutes,
       withRouterConfig({
@@ -17,5 +16,6 @@ export const appConfig: ApplicationConfig = {
     ),
     provideHttpClient(),
     provideAnimations(),
+    provideZonelessChangeDetection(),
   ],
 };
