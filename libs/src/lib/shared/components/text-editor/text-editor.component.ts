c28a8f3f428523ce9afa200c93df7cc1a5cdf282
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-07-15 15:57:47 -0500

    add readonly editor

--- libs/src/lib/shared/components/text-editor/text-editor.component.ts
+++ libs/src/lib/shared/components/text-editor/text-editor.component.ts
@@ -1,28 +1,47 @@
 import { CommonModule } from "@angular/common";
-import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  inject,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
-import { Editor, NgxEditorComponent, NgxEditorModule, Toolbar } from "ngx-editor";
+import { DomSanitizer } from "@angular/platform-browser";
+import { Editor, NgxEditorComponent, NgxEditorModule, toHTML, Toolbar } from "ngx-editor";
+import { Subscription } from "rxjs";
 import { NarratorComponent, NarratorStyle } from "../narrator/narrator.component";
 @Component({
   selector: "lib-text-editor",
   templateUrl: "./text-editor.component.html",
   styleUrls: ["./text-editor.component.scss"],
+  encapsulation: ViewEncapsulation.None,
   imports: [MatButtonModule, NgxEditorModule, CommonModule, NarratorComponent, ReactiveFormsModule],
 })
 export class TextEditorComponent implements OnInit, OnDestroy, AfterViewInit {
+  private sanitizer = inject(DomSanitizer);
+
   public narratorStyle = NarratorStyle;
   public editor!: Editor;
   public isReadMore = false;
   public isEmpty = false;
-  public className = `text-editor`;
+
+  private subscription!: Subscription;
 
   @Input() public document = {};
   @Input() public readonly = false;
   @Input() public showReadMore = true;
   @Output() public editorChanged = new EventEmitter();
 
-  @ViewChild("ngxeditor") public editorComponent!: NgxEditorComponent;
+  @ViewChild("ngxeditor") public editorComponent?: NgxEditorComponent;
+  @ViewChild("readonlyHtml", { read: ElementRef }) public readonlyEditor?: ElementRef;
 
   public toolbar: Toolbar = [
     ["bold", "italic"],
@@ -52,7 +71,7 @@ export class TextEditorComponent implements OnInit, OnDestroy, AfterViewInit {
       },
     });
 
-    this.editor.valueChanges.subscribe((value) => this.editorChanged.emit(value));
+    this.subscription = this.editor.valueChanges.subscribe((value) => this.editorChanged.emit(value));
 
     this.form = new FormGroup({
       editorContent: new FormControl({
@@ -63,11 +82,23 @@ export class TextEditorComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.editorComponent.setDisabledState(this.readonly);
+    this.editorComponent?.setDisabledState(this.readonly);
   }
 
   ngOnDestroy(): void {
     this.editor?.destroy();
+    this.subscription?.unsubscribe();
+  }
+
+  getText() {
+    return this.readonly
+      ? this.readonlyEditor?.nativeElement?.innerText
+      : this.editorComponent!.editor?.view?.dom?.innerText;
+  }
+
+  getReadonlyHtml() {
+    const html = toHTML(this.document);
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
   showText() {
