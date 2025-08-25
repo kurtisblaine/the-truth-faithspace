2d5f42ad80e936b910efe3e2d109efbb116e6cf0
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-08-24 21:02:06 -0500

    change to and

--- apps/quality-painting/src/app/app.component.ts
+++ apps/quality-painting/src/app/app.component.ts
@@ -29,7 +29,7 @@ import { SettingsWidgetComponent } from "./shared/component/settings-widget.comp
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  public title = "Dave's Drywall & Painting";
+  public title = "Dave's Drywall and Painting";
   public afterFirstRender = false;
   public isMobile = false;
 
