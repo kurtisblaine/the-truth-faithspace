04121c2bc1d646caa1c1b640580f0280b54d07c3
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2025-11-07 19:05:07 -0600

    fix the auto fetching system

---
+++ apps/bible/src/app/scripture-page/height-measure.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, Renderer2, RendererFactory2 } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ElementMeasureService {
+  private renderer: Renderer2;
+
+  constructor(rendererFactory: RendererFactory2, private sanitizer: DomSanitizer) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
+  /**
+   * Measures the height of a dynamically created element offscreen.
+   * @param tagName The HTML tag name of the element to create.
+   * @param content The content (text or HTML) to put inside the element.
+   * @param styles Optional CSS styles to apply for accurate measurement (e.g., width constraints).
+   * @returns The height in pixels.
+   */
+  measureElementHeight(tagName: string, content: string, styles?: { [key: string]: string }): number {
+    const element = this.renderer.createElement(tagName);
+    const trustworthyHtml = this.sanitizer.bypassSecurityTrustHtml(content);
+    this.renderer.setProperty(element, "innerHTML", trustworthyHtml);
+
+    this.renderer.setStyle(element, "position", "absolute");
+    this.renderer.setStyle(element, "left", "-9999px");
+    this.renderer.setStyle(element, "top", "-9999px");
+    this.renderer.setStyle(element, "visibility", "hidden");
+    this.renderer.setStyle(element, "height", "auto");
+
+    if (styles) {
+      Object.keys(styles).forEach((styleKey) => {
+        console.log(styles);
+        this.renderer.setStyle(element, styleKey, styles[styleKey]);
+      });
+    }
+
+    this.renderer.appendChild(document.body, element);
+
+    let { height } = element.getBoundingClientRect();
+    if (element.innerHTML.startsWith("SafeValue")) {
+      height = height - 27 * 2;
+    }
+
+    this.renderer.removeChild(document.body, element);
+    return height;
+  }
+}
--- apps/bible/src/app/scripture-page/scripture-page.component.ts
+++ apps/bible/src/app/scripture-page/scripture-page.component.ts
@@ -1,13 +1,36 @@
 import { CdkVirtualScrollViewport, ScrollingModule } from "@angular/cdk/scrolling";
 import { CommonModule } from "@angular/common";
-import { Component, OnDestroy, OnInit, Input as RouteInput, ViewChild } from "@angular/core";
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  computed,
+  OnDestroy,
+  OnInit,
+  Input as RouteInput,
+  signal,
+  ViewChild,
+} from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { MatRippleModule } from "@angular/material/core";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
-import { Observable, Subject, Subscription, bufferCount, filter, from, map, mergeMap } from "rxjs";
+import { CdkDynamicSizeVirtualScroll, itemDynamicSize } from "@rdlabo/ngx-cdk-scroll-strategies";
+import {
+  bufferCount,
+  distinctUntilChanged,
+  filter,
+  from,
+  map,
+  mergeMap,
+  Observable,
+  scan,
+  Subject,
+  Subscription,
+} from "rxjs";
+import { SafeHtmlPipe } from "shared";
 import { BibleApiService } from "../+state/bible-api.service";
 import { BooksActions } from "../+state/books/books.actions";
 import { selectEntity } from "../+state/books/books.selectors";
@@ -15,26 +38,43 @@ import { ChaptersActions } from "../+state/chapters/chapters.actions";
 import { selectChapterEntity, selectChaptersCount } from "../+state/chapters/chapters.selectors";
 import { Scripture } from "../models/scripture";
 import { MyDataSource } from "./data-source";
+import { ElementMeasureService } from "./height-measure.service";
 
+type DynamicScripture = { chapter: number; content: any; verseCount: number; refresh: boolean };
 @Component({
   selector: "app-scripture-page",
-  imports: [CommonModule, MatCardModule, ScrollingModule, MatRippleModule, MatDividerModule, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    ScrollingModule,
+    MatRippleModule,
+    MatDividerModule,
+    MatProgressSpinnerModule,
+    CdkDynamicSizeVirtualScroll,
+    SafeHtmlPipe,
+  ],
   templateUrl: "./scripture-page.component.html",
   styleUrl: "./scripture-page.component.scss",
   providers: [],
 })
-export class ScripturePageComponent implements OnInit, OnDestroy {
+export class ScripturePageComponent implements OnInit, OnDestroy, AfterViewInit {
   @RouteInput() public bibleId: string;
   @RouteInput() public bookId: string;
   @RouteInput() public languageName: string;
   @RouteInput() public chapterId: string;
 
-  @ViewChild(CdkVirtualScrollViewport, { static: true })
+  @ViewChild(CdkVirtualScrollViewport)
   private cdkVirtualScrollViewport!: CdkVirtualScrollViewport;
 
   public selectedBook$!: Observable<string>;
 
-  constructor(private store: Store, private router: Router, private bibleApi: BibleApiService) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private bibleApi: BibleApiService,
+    private measureService: ElementMeasureService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   public scripture$!: Observable<Scripture>;
   public isLoading$!: Observable<boolean>;
@@ -43,10 +83,36 @@ export class ScripturePageComponent implements OnInit, OnDestroy {
   public isAll!: boolean;
   public activeChapter!: string;
   public subscription!: Subscription;
-  public dataSubsciption!: Subscription;
+  public dataSubscription!: Subscription;
+  public scrollSubscription!: Subscription;
   public dataSource: MyDataSource;
+
   private requestQueue = new Subject<number>();
-  public items: { chapter: number; content: any; verseCount: number }[] = [];
+  private viewportWidth = signal(0);
+
+  private readonly initialFetchAmount = 2;
+
+  private readonly sectionMarginTop = 4;
+  private readonly headerMarginBottom = 14;
+  private readonly divider = 1;
+  private readonly headerDiv = 36;
+  private readonly headerSection = this.headerDiv + this.headerMarginBottom + this.divider;
+
+  public items = signal<DynamicScripture[]>([]);
+  readonly dynamicSize = computed<itemDynamicSize[]>(() =>
+    this.items()
+      .map((item) => ({
+        trackId: item.chapter,
+        itemSize: this.measureService.measureElementHeight("div", item.content, {
+          "line-height": "27px",
+          width: this.viewportWidth().toString() + "px",
+        }),
+      }))
+      .map((result) => ({
+        trackId: result.trackId,
+        itemSize: result.itemSize + this.headerSection + this.sectionMarginTop,
+      }))
+  );
 
   ngOnInit(): void {
     this.isAll = this.chapterId == "all";
@@ -61,28 +127,62 @@ export class ScripturePageComponent implements OnInit, OnDestroy {
 
         this.dataSource = new MyDataSource(this.bibleApi, this.bibleId, this.bookId, total);
 
-        this.dataSubsciption = this.requestQueue
+        this.dataSubscription = this.requestQueue
           .pipe(
-            filter((r) => {
-              return r < total;
-            }),
+            filter((r) => r < total),
             bufferCount(1),
-            mergeMap((requests) =>
-              from(requests).pipe(mergeMap((startIndex) => this.dataSource.fetchData(startIndex, 5)))
+            scan((acc, requests) => ({ count: acc.count + 1, requests }), { count: 0, requests: [] as number[] }),
+            mergeMap((data) =>
+              from(data.requests).pipe(
+                mergeMap((startIndex) =>
+                  this.dataSource.fetchData(startIndex, data.count === 1 ? this.initialFetchAmount : 1)
+                )
+              )
             )
           )
-          .subscribe((data: { chapter: number; content: any; refresh: boolean; verseCount: number }[]) => {
+          .subscribe((data: DynamicScripture[]) => {
             if (data.some((r) => r.refresh)) {
               const dataToRefresh = data.filter((r) => r.refresh);
-              this.items.splice(dataToRefresh[0].chapter, dataToRefresh.length, ...dataToRefresh);
+              this.items.update((currentItems) => [...currentItems, ...dataToRefresh]);
 
-              this.items = [...this.items];
+              //TODO - fix why this isn't updating!
+              // this.cdkVirtualScrollViewport.checkViewportSize();
+              // this.changeDetectorRef.detectChanges();
+              // (this.cdkVirtualScrollViewport as any)._changeDetectorRef.detectChanges();
             }
           });
       });
     } else {
       this.activeChapter = this.chapterId.split(".").pop();
       this.bibleApi.getScripture(this.bibleId, `${this.bookId}.${this.chapterId}`);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    const { width } = this.cdkVirtualScrollViewport.elementRef.nativeElement.getBoundingClientRect();
+    if (width) this.viewportWidth.set(width);
+
+    this.scrollSubscription = this.cdkVirtualScrollViewport
+      .elementScrolled()
+      .pipe(
+        map(() => this._getIndex()),
+        filter((index) => !!index),
+        distinctUntilChanged()
+      )
+      .subscribe((index) => this.onScrollIndexChange(index));
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
+    }
+
+    if (this.scrollSubscription) {
+      this.scrollSubscription.unsubscribe();
     }
   }
 
@@ -94,18 +194,19 @@ export class ScripturePageComponent implements OnInit, OnDestroy {
     this.requestQueue.next(index);
   }
 
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+  private _getIndex() {
+    const scrollHeight =
+      this.cdkVirtualScrollViewport.measureScrollOffset("top") +
+      Math.ceil(this.cdkVirtualScrollViewport.elementRef.nativeElement.getBoundingClientRect().height);
 
-    if (this.dataSubsciption) {
-      this.dataSubsciption.unsubscribe();
-    }
-  }
+    const totalHeight = this.dynamicSize().reduce((totalHeight, dynamicSize) => {
+      totalHeight += dynamicSize.itemSize;
+      return totalHeight;
+    }, 0);
 
-  calculateContainerHeight() {
-    this.cdkVirtualScrollViewport.checkViewportSize();
+    if (scrollHeight >= totalHeight) {
+      return this.dynamicSize().length;
+    }
   }
 
   public onPreviousArrowClick(scripture: Scripture) {
