67600329537f57869cb128898a4283d2f03a7557
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2026-04-05 19:28:31 -0500

    update draw page to be mobile friendly

--- apps/gospel/src/app/draw-page/draw-page.component.ts
+++ apps/gospel/src/app/draw-page/draw-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { afterNextRender, Component, HostListener, OnInit } from "@angular/core";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { SeoBaseComponent } from "shared";
 import { DataService } from "./data.service";
@@ -22,8 +22,16 @@ export class DrawPageComponent extends SeoBaseComponent implements OnInit {
   public images: Image[] = [];
   public faLink = faArrowUpRightFromSquare;
 
+  public columns: number = 4;
+  @HostListener("window:resize", ["$event"])
+  onResize(event: any) {
+    this.columns = event.target.innerWidth <= 600 ? 2 : event.target.innerWidth <= 800 ? 3 : 4;
+  }
+
   constructor(private dataService: DataService) {
     super();
+
+    afterNextRender(() => window.dispatchEvent(new Event("resize")));
   }
 
   ngOnInit(): void {
