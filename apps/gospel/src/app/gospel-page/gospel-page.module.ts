e351bd0f40ec3adbfef1bb621b5e0abe79bc8781
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2026-04-14 19:36:02 -0500

    hide menu from smaller devices.

--- apps/gospel/src/app/gospel-page/gospel-page.module.ts
+++ apps/gospel/src/app/gospel-page/gospel-page.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatExpansionModule } from "@angular/material/expansion";
+import { MatMenuModule } from "@angular/material/menu";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { RouterModule, Routes } from "@angular/router";
@@ -56,6 +57,7 @@ const routes: Routes = [
     GospelSectionComponent,
     DashToTitlePipe,
     PageHeaderComponent,
+    MatMenuModule,
   ],
   providers: [],
 })
