14622d91ff1a0bd90563e9582c57a48f7c5038e6
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2026-06-10 21:25:04 -0500

    add to stepper service proper steps and change to signals

--- apps/bible/src/app/app.component.ts
+++ apps/bible/src/app/app.component.ts
@@ -6,18 +6,20 @@ import {
   StepperSelectionEvent,
 } from "@angular/cdk/stepper";
 import { CommonModule } from "@angular/common";
-import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, OnDestroy, OnInit, Signal, ViewChild } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatStepper, MatStepperModule } from "@angular/material/stepper";
 import { Router, RouterModule } from "@angular/router";
 import { Store } from "@ngrx/store";
-import { map, Observable, tap } from "rxjs";
+import { map, Observable } from "rxjs";
 import * as BiblesActions from "./+state/bibles/bibles.actions";
 import { selectSelectedLanguage, selectSelectedTranslation } from "./+state/bibles/bibles.selectors";
 import { selectCurrentBook } from "./+state/books/books.selectors";
 import { selectChapter } from "./+state/chapters/chapters.selectors";
+import { Bible } from "./models/bibles";
+import { Book } from "./models/books";
 import { StepperStateService } from "./stepperState.service";
 @Component({
   imports: [
@@ -41,22 +43,15 @@ import { StepperStateService } from "./stepperState.service";
     position: sticky; width: 100%; top: 0; z-index: 7777;;
   }`,
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   public stepperOrientation$!: Observable<StepperOrientation>;
 
   @ViewChild("stepper") stepper!: MatStepper;
 
-  public selectedLanguage$!: Observable<string>;
-  private _selectedLanguageId: string;
-  public selectedTranslation$!: Observable<string>;
-  private _selectedTranslationId!: string;
-  public selectedBook$!: Observable<string>;
-  private _selectedBookId!: string;
-  public selectedChapter$!: Observable<string>;
-
-  ngAfterViewInit() {
-    this.stepperService.init(this.stepper);
-  }
+  public selectedLanguage!: Signal<string>;
+  public selectedTranslation!: Signal<Bible>;
+  public selectedBook!: Signal<Book>;
+  public selectedChapter!: Signal<string>;
 
   constructor(
     private router: Router,
@@ -70,21 +65,17 @@ export class AppComponent implements OnInit, AfterViewInit {
       .observe("(min-width: 800px)")
       .pipe(map(({ matches }) => (matches ? "horizontal" : "vertical")));
 
-    this.selectedLanguage$ = this.store
-      .select(selectSelectedLanguage)
-      .pipe(tap((language) => (this._selectedLanguageId = language)));
-    this.selectedTranslation$ = this.store.select(selectSelectedTranslation).pipe(
-      tap((translation) => (this._selectedTranslationId = translation?.id)),
-      map((translation) => translation?.name)
-    );
-    this.selectedBook$ = this.store.select(selectCurrentBook).pipe(
-      tap((book) => (this._selectedBookId = book?.id)),
-      map((book) => book?.name)
-    );
-    this.selectedChapter$ = this.store.select(selectChapter);
-
-    // this.router.navigate([""]); //go to home on refresh.
+    this.selectedLanguage = this.store.selectSignal(selectSelectedLanguage);
+    this.selectedTranslation = this.store.selectSignal(selectSelectedTranslation);
+    this.selectedBook = this.store.selectSignal(selectCurrentBook);
+    this.selectedChapter = this.store.selectSignal(selectChapter);
   }
+
+  ngAfterViewInit() {
+    this.stepperService.init(this.stepper);
+  }
+
+  ngOnDestroy(): void {}
 
   onSelectionChange(event: StepperSelectionEvent): void {
     switch (event.selectedIndex) {
@@ -92,15 +83,15 @@ export class AppComponent implements OnInit, AfterViewInit {
         this.router.navigateByUrl("/");
         break;
       case 1:
-        this.router.navigateByUrl(`/tongue/${this._selectedLanguageId}`);
+        this.router.navigateByUrl(`/tongue/${this.selectedLanguage()}`);
         this.store.dispatch(BiblesActions.loadBibles()); //although the data is already retrieved on initialization, we want to know when the bibles are navigated to.
         break;
       case 2:
-        this.router.navigateByUrl(`/tongue/${this._selectedLanguageId}/bible/${this._selectedTranslationId}`);
+        this.router.navigateByUrl(`/tongue/${this.selectedLanguage()}/bible/${this.selectedTranslation()?.id}`);
         break;
       case 3:
         this.router.navigateByUrl(
-          `/tongue/${this._selectedLanguageId}/bible/${this._selectedTranslationId}/book/${this._selectedBookId}`
+          `/tongue/${this.selectedLanguage()}/bible/${this.selectedTranslation()?.id}/book/${this.selectedBook()?.id}`
         );
         break;
     }
--- apps/bible/src/app/stepperState.service.ts
+++ apps/bible/src/app/stepperState.service.ts
@@ -1,6 +1,7 @@
 // stepper-state.service.ts
 import { Injectable } from "@angular/core";
 import { MatStepper } from "@angular/material/stepper";
+import { cloneDeep } from "lodash-es";
 
 @Injectable({
   providedIn: "root",
@@ -13,10 +14,14 @@ export class StepperStateService {
   }
 
   goToNextStep() {
+    const currentStep = cloneDeep(this.stepper.selectedIndex);
     this.stepper.next();
+    this.stepper.selectedIndex = currentStep + 1;
   }
 
   goToPreviousStep() {
+    const currentStep = cloneDeep(this.stepper.selectedIndex);
     this.stepper.previous();
+    this.stepper.selectedIndex = currentStep - 1;
   }
 }
