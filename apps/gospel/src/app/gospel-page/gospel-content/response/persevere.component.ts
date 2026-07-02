a4262d988cfbe0767c13dbe4feab99f8a54e8582
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2026-07-02 16:47:26 -0500

    fix spelling of remembrance

--- apps/gospel/src/app/gospel-page/gospel-content/response/persevere.component.ts
+++ apps/gospel/src/app/gospel-page/gospel-content/response/persevere.component.ts
@@ -129,7 +129,7 @@ import { GospelContentBaseComponent } from "../gospel-content.base.component";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersevereComponent extends GospelContentBaseComponent {
-  override name: string = "persevere-by-rememberance";
+  override name: string = "persevere-by-remembrance";
   public override description: string =
-    "The knowledge of the Lord Jesus is salvation; therefore, we must persevere by rememberance, considering what Jesus did for us on the cross.";
+    "The knowledge of the Lord Jesus is salvation; therefore, we must persevere by remembrance, considering what Jesus did for us on the cross.";
 }
