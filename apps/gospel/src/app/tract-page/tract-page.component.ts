01018af09f351ac98c6d76bb60048cbf550716b6
Kurtis Waldner <Kurtis.Waldner@trojans.dsu.edu>
2026-07-11 20:15:34 -0500

    download zip

--- apps/gospel/src/app/tract-page/tract-page.component.ts
+++ apps/gospel/src/app/tract-page/tract-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnDestroy } from "@angular/core";
 import { faBook, faBookmark, faCreditCard, faFileAudio } from "@fortawesome/free-solid-svg-icons";
+import { Subscription } from "rxjs";
 import { SeoBaseComponent } from "shared";
 
 @Component({
@@ -8,7 +10,7 @@ import { SeoBaseComponent } from "shared";
   styleUrl: "./tract-page.component.scss",
   standalone: false,
 })
-export class TractPageComponent extends SeoBaseComponent {
+export class TractPageComponent extends SeoBaseComponent implements OnDestroy {
   override keywords: string = "tracts, bookmarks, cards, reminder, print, truth, eternity";
 
   public zipFileIcon = faFileAudio;
@@ -16,7 +18,30 @@ export class TractPageComponent extends SeoBaseComponent {
   public bookmarkIcon = faBookmark;
   public cardIcon = faCreditCard;
 
-  constructor() {
+  private subscription: Subscription;
+
+  constructor(private httpClient: HttpClient) {
     super();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  downloadZip(filePath: string) {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+
+    this.subscription = this.httpClient.get(filePath, { responseType: "blob" }).subscribe((blob) => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "the-good-news-of-the-kingdom-of-god.zip";
+      a.click();
+      window.URL.revokeObjectURL(url);
+    });
   }
 }
